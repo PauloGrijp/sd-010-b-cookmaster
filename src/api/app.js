4419f4c173e139a71755b9155f690031e98542da
RafaMaths <rmathscoach@gmail.com>
2021-09-20 19:26:42 -0300

    add middleware static

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 
 const app = express();
+/* Adicionado de acordo com o readme
+// ./src/api/app.js
+
+const path = require('path');
+// ...
+
+// /images é o caminho/end-point da API onde as imagens estarão disponíveis
+// path.join(__dirname, '..', 'uploads') é o caminho da pasta onde o multer deve salvar suas imagens ao realizar o upload
+// a pasta `uploads` está em `./src/uploads` e não deve ser renomeada ou removida (assim como o arquivo `ratinho.jpg`)
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+
+// ...
+ */
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
