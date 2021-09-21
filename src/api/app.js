6cd3d810f42af8ade6a893f8d114008761292268
Mariana Savoldi <marianasavodi123@gmail.com>
2021-09-21 16:42:46 -0300

    Começando pelo começo (conexão e rota)

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/users');
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
