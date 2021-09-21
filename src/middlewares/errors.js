e5e6ba680fd61725a683f47c9d07dc135ded6f73
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-21 16:02:13 -0300

    adicionando o middleware de erros

---
+++ src/middlewares/errors.js
@@ -0,0 +1,8 @@
+module.exports = (err, _req, res, _next) => {
+  const statusCode = {
+    badRequest: 400,
+    conflict: 409,
+  };
+  const code = statusCode[err.name];
+  return res.status(code).json({ message: err.message });
+};
