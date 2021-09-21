fb594144c3aa76c6b6f988b32f158d998e5fc693
esmaben <felippecorrea2010@gmail.com>
2021-09-21 10:32:21 -0300

    DEV req1: Criando responses no controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,22 @@
+const statusCode = require('http-status-codes');
+const userService = require('../services/userService');
+
+const create = async (req, res) => {
+	const { name, email, password } = req.body;
+    const role = 'user';
+	const user = await userService.create({ name, email, password });
+	const { id } = user;
+
+	if (user.message) {
+		return res.status(statusCode.BAD_REQUEST).json(
+			{ message: user.message },
+		);
+	} 
+        res.status(statusCode.CONFLICT).json(
+			{ message: user.message },
+		);
+    
+	return res.status(statusCode.CREATED).json({ user: { name, email, role, _id: id } });
+};
+
+module.exports = { create };
