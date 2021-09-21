b6f6089f5d0dde3191991a082e490daff9343c29
Antenor <antenorzapata@gmail.com>
2021-09-21 15:32:43 -0300

    feat: add query to create admin users

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-// const path = require('path');
+const path = require('path');
 const userRouter = require('./routes/userRouter');
 const loginRouter = require('./routes/loginRouter');
 const recipesRouter = require('./routes/recipesRouter');
@@ -19,7 +19,7 @@ app.get('/', (request, response) => {
 app.use('/users', userRouter);
 app.use('/login', loginRouter);
 app.use('/recipes', recipesRouter);
-// app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.use((err, req, res, _next) => {
   handleError(err, res);
