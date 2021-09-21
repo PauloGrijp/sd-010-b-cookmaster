f6b27fe0e928c1ebd45adf1ac966605640a3cf62
esmaben <felippecorrea2010@gmail.com>
2021-09-21 11:38:31 -0300

    FIXED req1: chamando a funçao isValidEmail

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,28 +1,29 @@
 const usersModel = require('../models/userModel');
 
-const existField = (field) => {
-    if (!field) {
+const existField = (name, email, password) => {
+    if (!name || !email || !password) {
       return false;
     }
     return true;
 };
-
 const isValidEmail = (email) => {
-    const regexEmail = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+    const regexEmail = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/;
     if (!email.match(regexEmail)) {
         return false;
     }
     return true;
 };
 
-const create = async ({ name, email, password }) => {
+const create = async ({ name, email, password, role }) => {
+    const existFields = existField(name, email, password);
+    const validEmail = isValidEmail(email);
     const existingEmail = await usersModel.findByEmail(email);
-    if (!existField || !isValidEmail) {
+    if (!existFields || !validEmail) {
       return { message: 'Invalid entries. Try again' }; 
     }
     if (existingEmail) return { message: 'Email already exists' }; 
-    const { id } = await usersModel.create({ name, email, password });
-    return { id, name, password };
+    const { id } = await usersModel.create({ name, email, password, role });
+    return { id, name, password, role };
 };
 
 module.exports = { create };
