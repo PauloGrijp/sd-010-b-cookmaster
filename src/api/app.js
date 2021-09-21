c47c0ec12c11024883ecea08d46cb6a7fffb790a
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-09-21 15:44:47 -0300

    Initial commit

--- src/api/app.js
+++ src/api/app.js
@@ -6,6 +6,8 @@ const app = express();
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/users');
 // Não remover esse end-point, ele é necessário para o avaliador
 
 module.exports = app;
