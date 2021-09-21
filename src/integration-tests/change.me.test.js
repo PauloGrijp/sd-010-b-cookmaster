bdd2924f41b04bd71a877e1197670ef97d52f939
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-09-21 20:46:41 -0300

    req. 11

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -57,6 +57,47 @@ describe('Testa rota GET /recipes', () => {
       expect(response.body[0]).to.have.keys('_id', 'name', 'ingredients', 'preparation');
       expect(response.body[1]).to.have.keys('_id', 'name', 'ingredients', 'preparation');
     });
-
 	});
+
+  describe('Testa getRecipeById', () => {
+		const newRecipe = [
+			{
+				name: 'beijinho',
+				ingredients: 'leite em pó, leite condensado, açúcar e cravo',
+				preparation: 'misture o leite em pós com o leite condensado e faça bolinhas. passe no açúcar e coloque um cravo por cima',
+			},
+		];
+	
+    let response;
+		const DBServer = new MongoMemoryServer();
+
+		let connectionMock;
+
+		before(async () => {
+      const URLMock = await DBServer.getUri();
+      connectionMock = await MongoClient.connect(URLMock, { useNewUrlParser: true, useUnifiedTopology: true });
+      sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+			
+			await connectionMock.db('Cookmaster').collection('recipes').insertOne({ newRecipe });
+			recipes = await chai.request(server).get('/recipes');
+      response = await chai.request(server).get(`/recipes/${recipes.body[0]._id}`);
+		});
+
+		after(async () => {
+      await connectionMock.db('Cookmaster').collection('recipes').deleteMany({});
+			MongoClient.connect.restore();
+    });
+
+    it('retorna código status "200"', () => {
+      expect(response).to.have.status(200);
+    })
+
+    it('retorna um objeto', () => {
+    	expect(response.body).to.be.an('object')
+    })
+
+    it('a receita possui o seguinte nome', () => {
+      expect(response.body).to.have.keys('_id', 'newRecipe');
+    });		
+  });
 })
