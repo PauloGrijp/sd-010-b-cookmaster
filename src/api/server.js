3ffdd7c0f709c19504576c0a37a441b40110b44d
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-09-21 11:44:07 -0300

    create: logica inicial do server

--- src/api/server.js
+++ src/api/server.js
@@ -1,4 +1,12 @@
+const bodyParser = require('body-parser');
 const app = require('./app');
+const recipesRouter = require('../routers/recipesRouter');
+const usersRouter = require('../routers/usersRouter');
+
+app.use(bodyParser.json());
+
+app.use('/recipes', recipesRouter);
+app.use('/users', usersRouter);
 
 const PORT = 3000;
 
