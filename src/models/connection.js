fdd108407c0f35f3e6f07a76b75e948d488b1a79
RafaMaths <rmathscoach@gmail.com>
2021-09-21 09:58:21 -0300

    create connection in models

---
+++ src/models/connection.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+
+const DB_NAME = 'StoreManager';
+
+const connection = () => MongoClient
+    .connect(MONGO_DB_URL, {
+    //   urlNewParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME)) // nome do banco de dados
+    .catch((err) => {
+      console.error(err);
+      process.exit();
+    });
+
+module.exports = {
+    connection,
+};
