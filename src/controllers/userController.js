0391ca5e02f6b68ac32fd039ef7e8f3d2acd2396
esmaben <felippecorrea2010@gmail.com>
2021-09-21 10:40:25 -0300

    DEV req1: Criando rota POST  no app.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,7 @@ const create = async (req, res) => {
 	const user = await userService.create({ name, email, password });
 	const { id } = user;
 
-	if (user.message) {
+	if (user.message === 'Invalid entries. Try again') {
 		return res.status(statusCode.BAD_REQUEST).json(
 			{ message: user.message },
 		);
