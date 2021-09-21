eeec2177dc43f6f1510d49cca6cd4ac984f8b286
isTorres <ismaeltorres612@gmail.com>
2021-09-21 20:35:40 -0300

    set connection with mongodb

---
+++ src/models/connection.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+
+// local
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+// // git
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const DB_NAME = 'Cookmaster';
+
+let schema = null;
+
+const getConnection = async () => {
+  if (schema) return Promise.resolve(schema);
+  return MongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = getConnection;
