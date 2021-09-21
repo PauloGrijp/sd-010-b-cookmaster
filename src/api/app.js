aa8bb71c77ae9f2c35b5d399be1dd9f3a2de7d0e
esmaben <felippecorrea2010@gmail.com>
2021-09-21 11:39:14 -0300

    FIXED: adicionando o body-parser ao app.use

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const userController = require('../controllers/userController');
 
 const app = express();
-
+app.use(bodyparser.json());
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
