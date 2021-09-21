a74adcc038e2b1109d6c4449e47619f926ec37c4
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-09-21 19:55:52 -0300

    Criado a validação de email, se existe e se o formato é válido

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,13 +2,13 @@ const userValidations = require('../services/userValidations');
 
 const createUser = async (req, res) => {
   const { name, email, password, role } = req.body;
-  console.log(req.body);
   const { id, message } = await userValidations.createUserValidations({
     name, email, password, role,
   });
-  
-  // console.log(create);
-  return res.status(200).json({ _id: id, name, email, password, role: 'user' });
+  if (message) {
+    return res.status(400).json({ message });
+  }
+  return res.status(201).json({ user: { name, email, password, role: 'user', _id: id } });
 };
 
 module.exports = {
--- src/services/userValidations.js
+++ src/services/userValidations.js
@@ -7,10 +7,22 @@ const validateName = (name) => {
   return true;
 };
 
+const validateEmail = (email) => {
+  const regexEmail = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/;
+  if (!email || !regexEmail.test(email)) {
+    return false;
+  }
+  return true;
+};
+
 const createUserValidations = async ({ name, email, password, role }) => {
   const validName = validateName(name);
+  const validEmail = validateEmail(email);
   if (!validName) {
-    return { message: ' Invalid entries. Try again.' };
+    return { message: 'Invalid entries. Try again.' };
+  }
+  if (!validEmail) {
+    return { message: 'Invalid entries. Try again.' };
   }
   const create = await userModels.creatUser({ name, email, password, role });
   return create;
