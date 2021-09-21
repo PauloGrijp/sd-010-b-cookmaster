b5d2119e18d00131ec9c88cb40f72097e48ab881
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-21 17:14:38 -0300

    Feat: create app.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const usersRoute = require('../router/usersRoute');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => res.status(200).json({ message: 'I\'m feelin\' good!!!' }));
+app.use('/users', usersRoute);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
