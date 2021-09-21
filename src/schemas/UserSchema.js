1ad938f3c57324003abbd3f455280967e0bec3ac
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-21 18:52:09 -0300

    Refactor: change UserSchema from ifs to switch-case

--- src/schemas/UserSchema.js
+++ src/schemas/UserSchema.js
@@ -1,9 +1,15 @@
 const BAD_REQUEST = 400;
+const errors = {
+  nameBlank: 'Invalid entries. Try again.',
+};
+
+const blank = (value) => (!value);
 
 const validate = (name) => {
-  if (!name) return { code: BAD_REQUEST, message: 'Invalid entries. Try again.' };
-
-  return false;
+  switch (true) {
+    case blank(name): return { code: BAD_REQUEST, message: errors.nameBlank };
+    default: return false;
+  }
 };
 
 module.exports = {
