99be67b9de9bccb0c00f713b0b8ed312a065e273
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-09-21 15:03:32 -0300

    create connection mongodb

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.use();
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
