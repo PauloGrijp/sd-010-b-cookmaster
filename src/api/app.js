28985e77516d11b678a46534608161750b83becf
Jonathan Souza <souzajonathan570@gmail.com>
2021-09-21 14:16:53 -0300

    iniciando o projeto cookmaster

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const patch = require('path');
 
 const app = express();
+
+app.use('/images', express.static(patch.join(__dirname, '...', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
