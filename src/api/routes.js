f0113e7f3eea53ae32ed73f3f73c43892e5baf44
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-21 16:09:33 -0300

    agrupando as rotas

---
+++ src/api/routes.js
@@ -0,0 +1,9 @@
+const users = require('../routes/users');
+const login = require('../routes/login');
+const recipes = require('../routes/recipes');
+
+module.exports = {
+  users,
+  login,
+  recipes,
+};
