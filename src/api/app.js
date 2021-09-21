a0e3d43045d8c29c7ec0a837cf5046e2c0fca9cd
Caio <thehaseol@gmail.com>
2021-09-21 19:51:56 -0300

    Req 01

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const UserRouter = require('../rotes/Users');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
