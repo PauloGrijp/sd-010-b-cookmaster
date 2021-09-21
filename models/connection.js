4d62669496c3639186d0721ec03012d6ca03a3d6
esmaben <felippecorrea2010@gmail.com>
2021-09-21 09:09:57 -0300

    DEV: criando arquitetura MSC (pasta models com o arquivo connection, pasta services e pasta controllers)

---
+++ models/connection.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+const getConnection = () => MongoClient
+    .connect(MONGO_DB_URL, {
+      // urlNewParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME)) // nome do banco de dados
+    .catch((err) => {
+      console.error(err);
+      process.exit();
+    });
+
+module.exports = {
+    getConnection,
+};
