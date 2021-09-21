8bce40f64d9bf4baf8d91657ff470615d9dfaba3
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-21 16:11:04 -0300

    adicionando middlewares da aplicação

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const appRoutes = require('./routes');
+const handleErrors = require('../middlewares/errors');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/users', appRoutes.users);
+app.use(handleErrors);
 
 module.exports = app;
