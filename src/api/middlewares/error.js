b86e53880e447fe59277c8ba5d24eaa4b1df2fcd
André De Bem <andrebemferraz@gmail.com>
2021-09-21 17:26:50 -0300

    Request I - Fixed users test file

--- src/api/middlewares/error.js
+++ src/api/middlewares/error.js
@@ -1,17 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 
-module.exports = (err, req, res, _next) => {
-  // // Se for um erro do Joi, sabemos que trata-se de um erro de validação
-  // if (err.isJoi) {
-  //   // Logo, respondemos com o status 400 Bad Request
-  //   return res.status(400)
-  //     // E com a mensagem gerada pelo Joi
-  //     .json({ err: { message: err.details[0].message } });
-  // }
-
-  // Caso não seja um erro do Joi, pode ser um erro de domínio ou um erro inesperado.
-  // Construímos, então, um mapa que conecta um erro de domínio a um status HTTP.
-  
+module.exports = (err, req, res, _next) => {  
   if (!err.codeError) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: err.isErrorMessage });
   }
