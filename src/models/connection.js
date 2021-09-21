61b2d374401b0bb1567fcc52122f29759f06cb26
riquee <strikedv@hotmail.com>
2021-09-21 14:19:07 -0300

    connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,6 +1,9 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+require('dotenv/config');
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+
 const DB_NAME = 'Cookmaster';
 
 const OPTIONS = {
@@ -10,7 +13,8 @@ const OPTIONS = {
 
 let db = null;
 
-const connection = () => (db
+const connection = () =>
+  (db
     ? Promise.resolve(db)
     : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
         db = conn.db(DB_NAME);
