66973e16bda6ac8d401abc97239c44056735ee3d
Daniel <darkdanielr@gmail.com>
2021-09-21 17:29:36 -0300

    desafio1

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,6 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const routeUser = require('../reqs/routes/routeUser');
 
 const app = express();
 
@@ -7,5 +9,9 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(bodyParser.json());
+
+app.use('/users', routeUser);
 
 module.exports = app;
