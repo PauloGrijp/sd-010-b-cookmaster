f747743d65d04f584a640c802f0e5819238b1061
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-21 18:58:07 -0300

    adicionando a função authenticate e exportando as funções

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -6,4 +6,13 @@ const create = async (req, res) => {
   res.status(201).json({ user });
 };
 
-module.exports = createUser;
+const authenticate = async (req, res) => {
+  const { email, password } = req.body;
+  const token = await usersService.authenticate({ email, password });
+  res.status(200).json({ token });
+};
+
+module.exports = {
+  create,
+  authenticate,
+};
