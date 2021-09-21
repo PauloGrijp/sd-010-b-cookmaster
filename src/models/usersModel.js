a8db633f4c16b4489e642064ec6489e0f70b8530
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-21 16:03:20 -0300

    adicionando o usersModel

---
+++ src/models/usersModel.js
@@ -0,0 +1,23 @@
+const ErrorRequest = require('../helpers/errorRequest');
+const connection = require('./connection');
+
+const users = 'users';
+
+const existsEmail = async (email) => {
+  const db = await connection();
+  const result = await db.collection(users).findOne({ email });
+  if (result) throw new ErrorRequest('conflict', 'Email already registered');
+  return false;
+};
+
+const createUser = async ({ name, email, password }) => {
+  const db = await connection();
+  const result = await db.collection(users).insertOne({ name, email, password, role: 'user' });
+  const { _id, role } = result.ops[0];
+  return { _id, email, name, role };
+};
+
+module.exports = {
+  existsEmail,
+  createUser,
+};
