5f971db66736ec26c4740df571feaa7743314ba4
esmaben <felippecorrea2010@gmail.com>
2021-09-21 21:14:14 -0300

    FIXED: consertando a response da propridade message('JWT malformed) para 'jwt malformed'

--- src/middlewares/validateJWT.js
+++ src/middlewares/validateJWT.js
@@ -5,19 +5,19 @@ const segredo = 'seusecretdetoken';
 
 const validateJWT = async (req, res, next) => {
   const token = req.headers.authorization;
-  if (!token) { return res.status(401).json({ message: 'JWT malformed' }); }
+  if (!token) { return res.status(401).json({ message: 'malformed' }); }
   try {
     const decoded = jwt.verify(token, segredo);
     console.log(decoded);
     const user = await findUser({ email: decoded.data.email, password: decoded.data.password });
     if (!user) {
-        return res.status(401).json({ message: 'JWT malformed' }); 
+        return res.status(401).json({ message: 'JWT' }); 
     }
     req.user = user;
     console.log(req.user);
     next();
   } catch (_err) {
-    return res.status(401).json({ message: 'JWT malformed' });
+    return res.status(401).json({ message: 'jwt malformed' });
   }
 };
 
