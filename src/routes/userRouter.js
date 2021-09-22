e1f1bafc4d00d0cc53bd4b52a493154db65da907
Lara Capila <laracapilak@gmail.com>
2021-09-22 16:45:03 -0300

    Cria rota para /login

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController');
 const router = Router();
 
 router.post('/users', userController.createdUser);
+router.post('/login', userController.login);
 
 module.exports = router;
