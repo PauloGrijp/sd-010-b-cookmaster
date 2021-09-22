9c67461d9fb05be6c295e68b3f67e82fa49dfdd4
Roger Franco <rogerfrancogalt@gmail.com>
2021-09-22 00:13:11 -0400

    req 3

--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -4,6 +4,10 @@ const { createRecipe } = require('../controllers/recipeController');
 
 const routes = new Router();
 
+routes.get('/', (_request, response) => {
+  response.send();
+});
+
 routes.post('/recipes', validToken, validRecipe, createRecipe);
 
 module.exports = routes;
