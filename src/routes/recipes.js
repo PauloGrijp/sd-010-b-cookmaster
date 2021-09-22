96499a78d27109cc8e01721c9d7bc237797664ae
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-22 19:02:40 -0300

    adicionando a rota PUT /:id/image com middleware de upload

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const validateJWT = require('../auth/validateJWT');
-const { create, getAll, getById, update, deleteById } = require('../controllers/recipesController');
+const { create, getAll, getById, update, deleteById,
+  putImage } = require('../controllers/recipesController');
+const uploadMiddleware = require('../middlewares/upload');
 
 const router = express.Router();
+
+router.route('/:id/image')
+  .put(rescue(validateJWT), rescue(uploadMiddleware), rescue(putImage));
 
 router.route('/:id')
   .get(rescue(getById))
