1a56c5ba25f31c524093e616a407d6f54ce585e5
tonpasso <wellpasso@gmail.com>
2021-09-22 15:27:43 -0300

    mudando conexao

--- src/models/connection.js
+++ src/models/connection.js
@@ -6,12 +6,12 @@ const OPTIONS = {
 };
 
 // Local
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const DB_NAME = 'Cookmaster';
 
 // Git
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-// const DB_NAME = 'Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 let connection = null;
 
