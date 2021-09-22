905f7e649e52a9898ac714b1dc85f7670b325320
Lara Capila <laracapilak@gmail.com>
2021-09-21 21:05:27 -0300

    Configura rota post /users

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,17 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
+const userRouter = require('../routes/userRouter');
+
 const app = express();
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(userRouter);
 
 module.exports = app;
