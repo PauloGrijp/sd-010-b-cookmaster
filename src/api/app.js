4e3e76f1bbd243e3fb08ca101a187847d8d34dd9
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-22 17:09:57 -0300

    Feat: login route set

--- src/api/app.js
+++ src/api/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const usersRoute = require('../router/usersRoute');
+const loginRoute = require('../router/loginRoute');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (_req, res) => res.status(200).json({ message: 'I\'m feelin\' good!!!' }));
 app.use('/users', usersRoute);
+app.use('/login', loginRoute);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
