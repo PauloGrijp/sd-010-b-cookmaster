32fc482992bde075aca63112657e76cf1a41fe5a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-22 15:12:04 -0300

    adicionando a rota DELETE /:id com a função delete em /recipes

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const validateJWT = require('../auth/validateJWT');
-const { create, getAll, getById, update } = require('../controllers/recipesController');
+const { create, getAll, getById, update, deleteById } = require('../controllers/recipesController');
 
 const router = express.Router();
 
 router.route('/:id')
   .get(rescue(getById))
-  .put(rescue(validateJWT), rescue(update));
+  .put(rescue(validateJWT), rescue(update))
+  .delete(rescue(validateJWT), rescue(deleteById));
 
 router.route('/')
   .post(rescue(validateJWT), rescue(create))
