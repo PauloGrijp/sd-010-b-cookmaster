a65ec6e379e292d3b09158075c7c295c3e63b9be
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-09-22 00:30:41 -0300

    Feat: endpoint para o login de usuários

---
+++ src/controllers/login.js
@@ -0,0 +1,18 @@
+const { findUser } = require('../services/userService');
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(401).json({ message: 'All fields must be filled' });
+    }
+
+    const result = await findUser(email, password);
+
+    if (result === null) {
+      return res.status(401).json({ message: 'Incorrect username or password' });
+    }
+ 
+    return res.status(200).json(result);
+};
+
+module.exports = { login };
