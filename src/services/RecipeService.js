ba454ce961340ffe71ef20c6adcb2c4ab4ac1017
RafaMaths <rmathscoach@gmail.com>
2021-09-22 16:12:49 -0300

    create getAllRecipes in RecipeService

--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -20,6 +20,9 @@ const createRecipe = async ({ name, ingredients, preparation }) => {
   return { name, ingredients, preparation, id };
 };
 
+const getAllRecipes = async () => recipeModel.getAllRecipes();
+
 module.exports = {
   createRecipe,
+  getAllRecipes,
 };
