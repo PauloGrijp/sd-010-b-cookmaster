218e59fff27a1efcec86d715785b6bb3d54e388f
esmaben <felippecorrea2010@gmail.com>
2021-09-22 13:33:54 -0300

    DEV req6: Criando query mongo para o root

--- seed.js
+++ seed.js
@@ -1,1 +1,6 @@
 // colocar query do MongoDB
+/* const mongoConnection = require('./connection');
+const loginCollection = await mongoConnection.getConnection() */
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
+
+/* module.exports = root; */
