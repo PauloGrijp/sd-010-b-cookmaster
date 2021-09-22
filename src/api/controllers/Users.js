6bf43bf77c85cd13b6c79d7be087f3f727e179af
Joao Vanelli <vanellijoao@gmail.com>
2021-09-22 17:01:51 -0300

    fix Req1

--- src/api/controllers/Users.js
+++ src/api/controllers/Users.js
@@ -1,15 +1,30 @@
-const { StatusCodes: { CREATED, BAD_REQUEST, CONFLICT } } = require('http-status-codes');
+const {
+  StatusCodes: {
+    CREATED,
+    BAD_REQUEST,
+    CONFLICT,
+    INTERNAL_SERVER_ERROR,
+  },
+} = require('http-status-codes');
 const Users = require('../services/Users');
 
 const createUser = async (req, res) => {
-  const { name, email, password } = req.body;
-  const newUser = await Users.createUser(name, email, password);
-  console.log(newUser);
+  try {
+    const {
+      name,
+      email,
+      password,
+    } = req.body;
+    const newUser = await Users.createUser(name, email, password);
+    console.log(newUser);
 
-  if (newUser.conflict) return res.status(CONFLICT).json({ message: newUser.message });
-  if (newUser.message) return res.status(BAD_REQUEST).json(newUser);
+    if (newUser.conflict) return res.status(CONFLICT).json({ message: newUser.message });
+    if (newUser.message) return res.status(BAD_REQUEST).json(newUser);
 
-  return res.status(CREATED).json(newUser);
+    return res.status(CREATED).json(newUser);
+  } catch (error) {
+    return res.status(INTERNAL_SERVER_ERROR).send('Something went wrong');
+  }
 };
 
 module.exports = {
