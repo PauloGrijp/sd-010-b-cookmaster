04252b8bead58d58599d8b2450cbad4a2557bd1b
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-21 22:21:02 -0300

    adicionando o middleware de validação de token

---
+++ src/auth/validateJWT.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const ErrorRequest = require('../helpers/errorRequest');
+
+const secret = 'cookmaster';
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (decoded) {
+      const { _id } = decoded;
+      req.userId = _id;
+    }
+
+    if (err) throw new ErrorRequest('unauthorized', err.message);
+  });
+  next();
+};
