55cd47905eef736aa9301f6b7a0acfcdb6be14c1
esmaben <felippecorrea2010@gmail.com>
2021-09-21 21:12:24 -0300

    DEV req3: pegando id do usuario atraves de req.user

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -3,8 +3,10 @@ const recipeService = require('../services/recipeService');
 
 const create = async (req, res) => {
 	const { name, ingredients, preparation } = req.body;
+	const { _id: id } = req.user;
+	console.log(id);
 	const recipe = await recipeService.create({ name, ingredients, preparation });
-	const { id, _id } = recipe;
+	const { _id } = recipe;
 
 	if (recipe.message === 'Invalid entries. Try again.') {
 		return res.status(statusCode.BAD_REQUEST).json(
