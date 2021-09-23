059a92478e700628b745714588e0d1d24d542534
esmaben <felippecorrea2010@gmail.com>
2021-09-23 11:17:32 -0300

    DEV req9: criando estrutura inicial do codigo

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const path = require('path');
+const multer = require('multer');
+const FormData = require('form-data');
+const fs = require('fs');
 const userController = require('../controllers/userController');
 const loginController = require('../controllers/loginController');
 const recipesController = require('../controllers/recipeController');
@@ -14,6 +17,24 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+const storage = multer.diskStorage({
+  destination: (_req, file, callback) => {
+    console.log(file);
+    callback(null, 'uploads');
+  },
+  filename: (_req, file, callback) => 
+    // VALIDAÇÃO AQUI
+    callback(null, `${file.originalname}`),
+  
+});
+const upload = multer({ storage });
+const stream = fs.createReadStream('./meu-arquivo.txt');
+
+const formInfo = new FormData();
+formInfo.append('file', stream);
+const formHeader = formInfo.getHeaders();
+
 app.post('/users', userController.create);
 app.post('/login', loginController.login);
 app.post('/recipes', validateJWT, recipesController.create);
@@ -21,5 +42,8 @@ app.get('/recipes', recipesController.getAll);
 app.get('/recipes/:id', recipesController.getById);
 app.put('/recipes/:id', validateJWT, recipesController.update);
 app.delete('/recipes/:id', validateJWT, recipesController.exclude);
+app.put('/recipes/:id/image/', validateJWT, formInfo, upload.single('file'), (req, res) => 
+  // const { id } = req.params;
+   res.status(201).json({ message: 'Arquivo' }));
 
 module.exports = app;
