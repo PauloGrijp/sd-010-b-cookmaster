9a7f2d9352de59fd66432d38de2ed87b91a33185
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-23 16:20:45 -0300

    Fix: sign({ data: items })

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -9,11 +9,11 @@ const jwtConfig = {
 
 const logUser = async (req, res) => {
   const { email, password } = req.body;
-  const { code, message } = await LoginService.findUser({ email, password });
+  const { code, message, _id, name, role } = await LoginService.findUser({ email, password });
 
   if (code) return res.status(code).json({ message });
 
-  const token = jwt.sign({ data: email }, secret, jwtConfig);
+  const token = jwt.sign({ data: { _id, name, email, role } }, secret, jwtConfig);
   res.status(200).json({ token });
 };
 
