ccb5e415c18016612c835a766cc50f9a23edf94d
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-23 12:44:22 -0300

    Requisito 1, Corrigindo bugs, Prestar mais atençao as imagens do README

--- src/api/models/modelUsers.js
+++ src/api/models/modelUsers.js
@@ -3,7 +3,7 @@ const connection = require('./connection');
 const collection = async () => connection()
   .then((db) => db.collection('users'));
 
-const create = async (name, email, password, role) => collection()
+const create = async (name, email, password, role = 'user') => collection()
   .then((col) => col.insertOne({ name, email, password, role }));
 
 const getByEmail = async (email) => collection()
