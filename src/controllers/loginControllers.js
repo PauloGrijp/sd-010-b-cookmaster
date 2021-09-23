4e992e090ef08b737c5029985c5572c7840ee1f8
EderPaiva <edergomesdepaiva12@gmail.com>
2021-09-23 07:04:51 -0300

    requisito 2

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,13 +1,25 @@
+const jwt = require('jsonwebtoken');
 const userModel = require('../models/usersModel');
 
 const STATUS_OK = {
     OK: 200,
 };
 
-const loginUser = async (req, res) => {
-    const { name, email } = req.body;
+const secret = 'programadorNaoTemFeriadoNemFinalDeSemana';
+const jwtConfigurations = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+};
 
-   return res.status(STATUS_OK.OK).json();
+const loginUser = async (req, res) => {
+    const { email } = req.body;
+    // Falta usuario sem a senha!
+    const userInfos = await userModel.findUserByEmail(email);
+    delete userInfos.password;
+
+    const token = jwt.sign({ data: userInfos }, secret, jwtConfigurations);
+
+   return res.status(STATUS_OK.OK).json({ token });
 };
 
 module.exports = {
