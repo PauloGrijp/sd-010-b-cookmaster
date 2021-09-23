18ef7b85d7a24d5acfb9add8ea8e66073c5416a7
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-23 16:51:50 -0300

    Taking off users model

--- src/models/usersModel.js
+++
@@ -1,9 +0,0 @@
-const connection = require('./connection');
-
-const postUser = async (name, email, password) => connection()
-  .then((db) => db.collection('users').insertOne({ name, email, password, role: 'user' }));
-
-const findEmail = async (email) => connection()
-  .then((db) => db.collection('users').findOne({ email }));
-
-module.exports = { postUser, findEmail };
