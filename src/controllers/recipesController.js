3dc7484a574af4b97e172d44331e7e79f7feb46b
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-23 16:17:42 -0300

    Fix: req.user

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -2,7 +2,8 @@ const RecipesService = require('../services/recipesService');
 
 const create = async (req, res) => {
   const recipeInput = req.body;
-  const { users: { _id } } = req.user;
+  console.log('req user: ', req.user);
+  const { _id } = req.user;
 
   const newRecipe = await RecipesService.create(recipeInput, _id);
   res.status(201).json(newRecipe);
