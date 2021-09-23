3b4aea31f1aad66794f87161dc17974ebba7975c
Cristian Bugs <cristian.bugs@gmail.com>
2021-09-22 23:43:55 -0300

    change connection

--- src/models/connections.js
+++ src/models/connections.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require('mongodb');
 
 // github
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-// const DB_NAME = 'Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 // testar localmente
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const DB_NAME = 'Cookmaster';
 
 let db = null;
 
