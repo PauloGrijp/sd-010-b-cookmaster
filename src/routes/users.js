ec75530ba7bfb000976b445f9843efd9327effe7
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-09-23 17:24:11 +0200

    New requirements 9, 10 and 12 and connection correction

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,10 @@
 const rescue = require('express-rescue');
 const controller = require('../controllers/users');
+const { tokenValidation } = require('../middlewares/token');
 
 function users(app) {
+  app.route('/users/admin')
+    .post(rescue(tokenValidation), rescue(controller.newUser));
   app.route('/users')
     .post(rescue(controller.newUser));
   app.route('/login')
