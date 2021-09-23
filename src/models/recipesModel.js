2a14b06be6412c3abc8686084a73f9e1e82cce91
Lara Capila <laracapilak@gmail.com>
2021-09-23 15:23:36 -0300

    Função que busca no banco receita por id

--- src/models/recipesModel.js
+++ src/models/recipesModel.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./mongoConnection');
 
 const COLLECTION = 'recipes';
@@ -23,7 +24,15 @@ const getAll = async () => {
   return recipes;
 };
 
+const getById = async (id) => {
+  const db = await connection();
+  const recipe = await db.collection(COLLECTION).findOne({ _id: ObjectId(id) });
+
+  return recipe;
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
