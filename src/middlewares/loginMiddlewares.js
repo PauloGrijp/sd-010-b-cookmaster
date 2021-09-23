51b423888485b277c328619ab041e6d506a99888
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-23 15:35:14 -0300

    Feat: Cria middleware de validação de login

---
+++ src/middlewares/loginMiddlewares.js
@@ -0,0 +1,21 @@
+const { unauthorized, unauthorizedEmailPassword } = require('../error/errorUsers');
+const Users = require('../models/usersModel');
+
+const checkLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+  const userEmail = await Users.findEmail(email);
+  const regex = /\S+@\S+\.\S+/;
+  
+  if (!regex.test(email) || !password) {
+    unauthorizedEmailPassword(res);
+  }
+
+  if (!userEmail || userEmail.password !== password) {
+    unauthorized(res);
+  }
+  next();
+};
+
+module.exports = {
+  checkLogin,
+};
