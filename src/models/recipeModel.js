ea670b109d3f188efc0e6a7bfd337123790a422b
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-09-23 07:00:14 -0300

    requisito 5 e 7

--- src/models/recipeModel.js
+++ src/models/recipeModel.js
@@ -28,7 +28,7 @@ const add = async (name, ingredients, preparation, id) => {
 
 // READ
 const getId = async (id) => {
-  console.log(id);
+  if (!ObjectId.isValid(id)) { return null; }
   const db = await connectionDB.connect();
   const productOne = await db.collection('recipes').findOne({ _id: ObjectId(id) });
   return productOne;
@@ -43,6 +43,7 @@ const getAll = async () => {
 
 // UPDATE
 const update = async ({ name, ingredients, preparation, id, userId }) => {
+  if (!ObjectId.isValid(id)) { return null; }
   const db = await connectionDB.connect();
   await db.collection('products')
   .updateOne({ _id: ObjectId(id) }, 
