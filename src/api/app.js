1eb1c74f4b0e0d1ed5a1008c1e0b2e42f49b1b54
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-22 21:52:02 -0300

    Place express.json to be able to read request body

--- src/api/app.js
+++ src/api/app.js
@@ -4,6 +4,8 @@ const middlewares = require('../middlewares');
 
 const app = express();
 
+app.use(express.json());
+
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
