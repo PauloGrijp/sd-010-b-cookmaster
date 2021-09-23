b788bddee06d12df230fa1a8793f51640a2aba51
isTorres <ismaeltorres612@gmail.com>
2021-09-23 19:04:09 -0300

    response of requests create and read recipes(controller)

---
+++ src/controllers/recipes.js
@@ -0,0 +1,24 @@
+const RecipeService = require('../services/recipes');
+
+const create = async (req, res) => {
+  const newRecipe = req.body;
+  const { authorization } = req.headers;
+  // console.log(authorization, 'token');
+
+  const createRecipe = await RecipeService.create(newRecipe, authorization);
+  if (createRecipe.err) return res.status(401).json(createRecipe.err);
+  if (createRecipe === false) {
+    return res.status(400).json({ message: 'Invalid entries. Try again.' }); 
+  }
+  return res.status(201).json({ recipe: createRecipe });
+};
+
+const getAllRecipes = async (_req, res) => {
+  const recipes = await RecipeService.getAllRecipes();
+  return res.status(200).json(recipes);
+};
+
+module.exports = {
+  create,
+  getAllRecipes,
+};
