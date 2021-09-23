cf307bbbee7be9d3a1885458a377907f0e649f5b
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-23 15:53:37 -0300

    Feat: Passando teste

--- src/middlewares/loginMiddlewares.js
+++ src/middlewares/loginMiddlewares.js
@@ -5,11 +5,9 @@ const checkLogin = async (req, res, next) => {
   const { email, password } = req.body;
   const userEmail = await Users.findEmail(email);
   const regex = /\S+@\S+\.\S+/;
-  
   if (!regex.test(email) || !password) {
     unauthorizedEmailPassword(res);
   }
-
   if (!userEmail || userEmail.password !== password) {
     unauthorized(res);
   }
