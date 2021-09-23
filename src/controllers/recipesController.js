20005d0a6a5d4cfe6c6157ba7db76e69549cee42
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-23 17:15:24 -0300

    Feat: recipesController getAll func

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -8,6 +8,12 @@ const create = async (req, res) => {
   res.status(201).json(newRecipe);
 };
 
+const getAll = async (_req, res) => {
+  const allRecipes = await RecipesService.getAll();
+  res.status(200).json(allRecipes);
+};
+
 module.exports = {
   create,
+  getAll,
 };
