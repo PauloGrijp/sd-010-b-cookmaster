4f0e48f2fa699405ffa24b46cc1f9e6adcf1094b
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-23 15:37:09 -0300

    Feat: Cria service login / busca usuario no banco

---
+++ src/services/loginServices.js
@@ -0,0 +1,10 @@
+const Users = require('../models/usersModel');
+
+const findUser = async (email) => {
+  const check = await Users.findEmail(email);
+  return check; 
+};
+
+module.exports = {
+  findUser,
+};
