c2f04842acdaadc5777e82a40a45a4421f6ad13d
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-23 17:21:24 -0300

    Feat: get recipe by ID route and requirement 4 done

--- src/router/recipesRoute.js
+++ src/router/recipesRoute.js
@@ -4,5 +4,6 @@ const { tokenValidator } = require('../middlewares/tokenValidator');
 const { validateInputs } = require('../middlewares/recipesMiddleware');
 
 router.post('/', tokenValidator, validateInputs, RecipesController.create);
+router.get('/', RecipesController.getAll);
 
 module.exports = router;
