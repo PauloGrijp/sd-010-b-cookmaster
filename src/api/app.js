65373732eaa4838ee5f41ee703d23e7a2af55cb1
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-23 16:33:40 -0300

    Feat: add recipes route

--- src/api/app.js
+++ src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const usersRoute = require('../router/usersRoute');
 const loginRoute = require('../router/loginRoute');
+const recipesRoute = require('../router/recipesRoute');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 app.get('/', (_req, res) => res.status(200).json({ message: 'I\'m feelin\' good!!!' }));
 app.use('/users', usersRoute);
 app.use('/login', loginRoute);
+app.use('/recipes', recipesRoute);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
