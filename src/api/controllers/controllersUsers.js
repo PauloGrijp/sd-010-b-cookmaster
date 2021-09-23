e7a16b637922c61395d4c6752a25b06ca7a05a28
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-23 11:31:02 -0300

    Requisito 1, Corrigindo bugs

--- src/api/controllers/controllersUsers.js
+++ src/api/controllers/controllersUsers.js
@@ -1,6 +1,6 @@
 const services = require('../services/servicesUsers');
 
 const create = async (req, res) => services.create(req.body)
-  .then(({ status, data }) => res.status(status).json(data));
+  .then(({ status, user }) => res.status(status).json(user));
 
 module.exports = { create };
