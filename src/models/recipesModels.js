9ffae83191f89a1f13e1fb98f9458d1971b77046
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-09-23 17:41:54 -0300

    Fixed error lint

--- src/models/recipesModels.js
+++ src/models/recipesModels.js
@@ -25,8 +25,7 @@ const getById = async ({ id }) => {
 
 const update = async ({ id, name, ingredients, preparation, userId }) => {
   const db = await connection();
-  const resultQuery = await db.collection('recipes')
-    .updateOne({ _id: ObjectId(id) }, { $set: { name, ingredients, preparation } });
+  await db.collection('recipes').updateOne({ _id: ObjectId(id) }, { $set: { name, ingredients, preparation } });
 
   return {
     _id: id, name, ingredients, preparation, userId,
