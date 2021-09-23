59d5f6e3c469a7eb74ad3d6b51e059da60bf4b15
Mariana Mohr <marianamohr@gmail.com>
2021-09-23 15:09:21 -0300

    Req 10 - feito

--- src/api/server.js
+++ src/api/server.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const app = require('./app');
 const usersRouter = require('../Routes/usersRouter');
 const loginRouter = require('../Routes/loginRouter');
@@ -7,5 +8,7 @@ const PORT = 3000;
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/recipes', recipesRouter);
+// https://github.com/tryber/sd-10b-live-lectures/blob/lecture/28.2/codigo/api/index.js
+app.use('/images', express.static(`${__dirname}/../uploads`));
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
