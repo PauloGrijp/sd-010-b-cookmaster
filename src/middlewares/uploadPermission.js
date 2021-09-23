f248997633d9f8277221179e04cb005e530f9665
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-09-23 11:15:44 -0300

    fix uploadpermission lint

--- src/middlewares/uploadPermission.js
+++ src/middlewares/uploadPermission.js
@@ -1,11 +1,11 @@
-const recipesModel = require('../models/recipes');
+// const recipesModel = require('../models/recipes');
 
-module.exports = async (req, res, next) => {
+module.exports = async (req, res, _next) => {
   const { id } = req.params;
   const { user } = req.body;
   const { _id: userId } = user;
 
-  const recipe = await recipesModel.getRecipeById(id);
+  // const recipe = await recipesModel.getRecipeById(id);
 
   console.log('uploads', id);
   console.log('uploads', userId);
