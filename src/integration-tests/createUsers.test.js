0eb6d026dd5a0c3ea64b0590d1d92cee7d1b5e32
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-09-23 16:48:50 -0300

    integration test for creating user with success

---
+++ src/integration-tests/createUsers.test.js
@@ -0,0 +1,53 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const sinon = require("sinon");
+const { MongoClient, ObjectId } = require("mongodb");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+const server = require("../api/app.js");
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+
+
+  describe("02 - quando é criado com sucesso:", () => {
+    let response = {};
+    const DBServer = new MongoMemoryServer();
+    const SEND_USER = {
+      name: "Erick Jacquin",
+      email: "erickjaquin@gmail.com",
+      password: "12345678",
+    };
+
+    before(async () => {
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(URLMock, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      sinon.stub(MongoClient, "connect").resolves(connectionMock);
+
+      response = await chai.request(server).post("/users").send(SEND_USER);
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+
+    it("retorna o código de status 201,", () => {
+      expect(response).to.have.status(201);
+    });
+
+    it("retorna um objeto,", () => {
+      expect(response.body).to.be.a("object");
+    });
+
+    it('o objeto possui a propriedade "user",', () => {
+      expect(response.body).to.have.property("user");
+    });
+  });
+});
