1e3196b360dad101aa83e8b5a909aade4d697209
Thiago Leite <leite.tjs@gmail.com>
2021-09-23 17:13:28 -0300

    subindo a fimde testes

--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -4,11 +4,16 @@ const SECRET = 'secret';
 
 const verifyToken = (req, res, next) => {
   const { authorization } = req.headers;
+
+  console.log(authorization);
   
   try {
-    jwt.verify(authorization, SECRET); 
+    const decode = jwt.verify(authorization, SECRET);
+    req.user = decode;
+    console.log(decode);
     next();
   } catch (err) {
+    console.log(err.message);
     return res.status(401).json({ message: 'jwt malformed' });
   }
 };
