2c7a184c12dde905f90c5c277356eb072cefbb67
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-09-23 00:50:04 -0300

    Fixing seeds.js

--- seed.js
+++ seed.js
@@ -1,4 +1,2 @@
 // colocar query do MongoDB
-use('Cookmaster')
-
-db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' })
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
