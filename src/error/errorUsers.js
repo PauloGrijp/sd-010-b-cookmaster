c896009d1c347a67f74f45cb1a8158425f2795cf
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-23 15:34:24 -0300

    Feat: cria funcões de erros para a rota login

--- src/error/errorUsers.js
+++ src/error/errorUsers.js
@@ -5,13 +5,28 @@ function badRequest(response) {
     message: 'Invalid entries. Try again.',
   });
 }
+
 function conflict(response) {
   return response.status(StatusCodes.CONFLICT).json({
     message: 'Email already registered',
   });
 }
 
+function unauthorizedEmailPassword(response) {
+  return response.status(StatusCodes.UNAUTHORIZED).json({
+    message: 'All fields must be filled',
+  });
+}
+
+function unauthorized(response) {
+  return response.status(StatusCodes.UNAUTHORIZED).json({
+    message: 'Incorrect username or password',
+  });
+}
+
 module.exports = {
   badRequest,
   conflict,
+  unauthorizedEmailPassword,
+  unauthorized,
 };
