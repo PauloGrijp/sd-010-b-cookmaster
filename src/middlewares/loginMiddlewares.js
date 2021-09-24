856cd881321ec64ae2bbce22159ffe74f60489d8
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-24 11:26:09 -0300

    Reafctor: Preparando para refatoração

--- src/middlewares/loginMiddlewares.js
+++ src/middlewares/loginMiddlewares.js
@@ -1,31 +1,31 @@
-const { unauthorized, unauthorizedEmailPassword } = require('../error/errorUsers');
-const Users = require('../models/usersModel');
+// const { unauthorized, unauthorizedEmailPassword } = require('../error/errorUsers');
+// const Users = require('../models/usersModel');
 
-const checkLogin = async (req, res, next) => {
-  const { email, password } = req.body;
-  const userEmail = await Users.findEmail(email);
+// const checkLogin = async (req, res, next) => {
+//   const { email, password } = req.body;
+//   const userEmail = await Users.findEmail(email);
 
-  if (!userEmail || userEmail.password !== password) {
-    unauthorized(res);
-  }
-  next();
-};
+//   if (!userEmail || userEmail.password !== password) {
+//     unauthorized(res);
+//   }
+//   next();
+// };
 
-const validEmailPassword = (req, res, next) => {
-  const { email, password } = req.body;
-  const regex = /\S+@\S+\.\S+/;
+// const validEmailPassword = (req, res, next) => {
+//   const { email, password } = req.body;
+//   const regex = /\S+@\S+\.\S+/;
 
-  if (!email || !password) {
-    unauthorizedEmailPassword(res);
-  }
+//   if (!email || !password) {
+//     unauthorizedEmailPassword(res);
+//   }
 
-  if (!regex.test(email)) {
-    unauthorized(res);
-  }
-  next();
-};
+//   if (!regex.test(email)) {
+//     unauthorized(res);
+//   }
+//   next();
+// };
 
-module.exports = {
-  checkLogin,
-  validEmailPassword,
-};
+// module.exports = {
+//   checkLogin,
+//   validEmailPassword,
+// };
