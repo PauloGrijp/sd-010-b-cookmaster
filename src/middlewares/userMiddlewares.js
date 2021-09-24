5c6c17039512fccae1f9b9c4d319d20a7786be05
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-24 11:27:02 -0300

    Refactor: Middleware de validação de dados do novo usuario

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -1,39 +1,16 @@
-const { badRequest, conflict } = require('../error/errorUsers');
-const Users = require('../models/usersModel');
+const { StatusCodes } = require('http-status-codes');
+const validate = require('../helpers/validateUser');
 
-const checkName = (req, res, next) => {
-  const { name } = req.body; 
-  if (!name) {
-    badRequest(res);
-  }
-  next();
-};
-
-const checkPasword = (req, res, next) => {
-  const { password } = req.body;
-  if (!password) {
-    badRequest(res);
-  }
-  next();
-};
-
-const checkEmail = async (req, res, next) => {
-  const { email } = req.body;
-  const userEmail = await Users.findEmail(email);
-  const regex = /\S+@\S+\.\S+/;
-  
-  if (!regex.test(email)) {
-    badRequest(res);
-  }
-
-  if (userEmail) {
-    conflict(res);
+const userValidate = (req, res, next) => {
+  const { error } = validate.schemaUser.validate(req.body);
+  if (error) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: error.details[0].message,
+    });
   }
   next();
 };
 
 module.exports = {
-  checkName,
-  checkPasword,
-  checkEmail,
+  userValidate,
 };
