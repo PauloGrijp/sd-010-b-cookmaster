92f326e782cafe247775b700767218fce6ff6046
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-24 11:22:50 -0300

    Refactor: reamove os routers/adiciona middlewares app

--- src/api/app.js
+++ src/api/app.js
@@ -1,20 +1,18 @@
 const express = require('express');
 
 const app = express();
-
 const bodyParser = require('body-parser');
-const usersRouter = require('../routers/usersRouter');
-const loginRouter = require('../routers/loginRouter');
+const users = require('../controllers/userController');
 
 app.use(bodyParser.json());
 
-// Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
-// Não remover esse end-point, ele é necessário para o avaliador
 
-app.use(usersRouter);
-app.use(loginRouter);
+const { userValidate } = require('../middlewares/userMiddlewares');
+
+app.use('/users', userValidate, users);
+// app.use(loginRouter);
 
 module.exports = app;
