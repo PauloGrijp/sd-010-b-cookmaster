9e815398a294a108144e41623f19d7918a4e9f9e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-24 14:13:57 -0300

    Refactor: Remove mensagem do schema de validação

--- src/helpers/validateUser.js
+++ src/helpers/validateUser.js
@@ -1,14 +1,9 @@
 const Joi = require('@hapi/joi');
 
-const MESSAGE = 'Invalid entries. Try again.';
-
 const schemaUser = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
-}).messages({
-  'any.required': MESSAGE,
-  'string.email': MESSAGE,
 });
 
 module.exports = {
