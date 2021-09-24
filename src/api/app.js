7d95cbdcb9e8fdd573eb016783884513b0e86aea
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-24 14:11:55 -0300

    Feat: Cria a rota login e  adiciona middlewares de validação

--- src/api/app.js
+++ src/api/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const users = require('../controllers/userController');
+const login = require('../controllers/loginController');
 
 app.use(bodyParser.json());
 
@@ -11,8 +12,9 @@ app.get('/', (request, response) => {
 });
 
 const { userValidate } = require('../middlewares/userMiddlewares');
+const { validateEmailPassword } = require('../middlewares/loginMiddlewares');
 
 app.use('/users', userValidate, users);
-// app.use(loginRouter);
+app.use('/login', validateEmailPassword, login);
 
 module.exports = app;
