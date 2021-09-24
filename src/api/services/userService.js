66c018f80c44789655c7567de9b09c8ac53789d4
antonioarieiro <sfgfffs@gmail.com>
2021-09-24 10:17:53 -0300

    rescue adicionado e schema de validacao do user feito

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,17 +1,23 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const Joi = require('@hapi/joi');
 const userModel = require('../models/userModel');
+
+const userSchm = Joi.object({
+  email: Joi.string().email().required(),
+  name: Joi.string().min(1).required(),
+  password: Joi.required(),
+});
 
 const createUser = async (email, name, password) => {
   const { error } = userSchm.validate({ email, name, password });
   if (error) {
     throw dataErr(400, 'Invalid entries. Try again.');
   }
-  const getUser = await User.getUserByEmail(email);
-  if (getUser) {
+  const getUserByEmailVal = await userModel.getUserByEmail(email);
+  if (getUserByEmailVal) {
     throw dataErr(409, 'Email already registered');
   }
-  const user = await userModel.createNewUser(email, name, password);
+  const user = await userModel.createUser(email, name, password);
   return { user };
 };
 module.exports = {
