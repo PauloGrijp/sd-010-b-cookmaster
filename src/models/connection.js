fa8536cf2b8d7f554bcb56a0022cdb9a91f825b4
isTorres <ismaeltorres612@gmail.com>
2021-09-24 00:46:19 -0300

    set url mongodb

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require('mongodb');
 
 // local
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const DB_NAME = 'Cookmaster';
 
 // // git
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-// const DB_NAME = 'Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 let schema = null;
 
