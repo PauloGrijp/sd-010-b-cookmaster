21f89849cce8640996a18805d529e8ffbc9a9c7c
antonioarieiro <sfgfffs@gmail.com>
2021-09-24 10:18:17 -0300

    adicionado o bodyParser

--- src/api/app.js
+++ src/api/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const userController = require('./controllers/userController');
+const bodyParser = require('body-parser').json();
+const User = require('./controllers/userController');
 
 const app = express();
+
+app.use(bodyParser);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
-app.post('/users', userController.createUser);
+
+/* rota para users */
+app.post('/users', User.createUser);
 
 module.exports = app;
