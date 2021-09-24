31007b12476238f603a17a33f71c4e738bdbc70e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-24 14:18:14 -0300

    Refactor: Melhora o middlewares de validação de ovos usuarios

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -1,11 +1,12 @@
-const { StatusCodes } = require('http-status-codes');
 const validate = require('../helpers/validateUser');
+const Error = require('../helpers/errorUsers');
 
 const userValidate = (req, res, next) => {
   const { error } = validate.schemaUser.validate(req.body);
+  const { message, code } = Error.badRequest('Invalid entries. Try again.'); 
   if (error) {
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      message: error.details[0].message,
+    return res.status(code).json({
+      message,
     });
   }
   next();
