b76adff567a0e1f7839dd9498b50f3aec6aced93
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-24 17:47:31 -0300

    Feat: Cria a rota no controller para visualizar todas receitas/adiciona middlewares que foi retirada do app

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -3,13 +3,24 @@ const rescue = require('express-rescue');
 const { StatusCodes } = require('http-status-codes');
 const Recipes = require('../services/recipesService');
 
+const recipesValidate = require('../middlewares/recipesMiddlewares');
+const validateJWT = require('../middlewares/validateJWT');
+
 const recipes = express.Router();
 
-recipes.post('/', rescue(async (req, res) => {
-  const { name, ingredients, preparation } = req.body;
-  const { _id } = req.user;
-  const recipe = await Recipes.create({ name, ingredients, preparation, userId: _id });
-  return res.status(StatusCodes.CREATED).json({ recipe });
+recipes.get('/', rescue(async (req, res) => {
+  const recipesAll = await Recipes.getAll();
+  return res.status(StatusCodes.OK).json(recipesAll);
+}));
+
+recipes.post('/',
+  recipesValidate,
+  validateJWT,
+  rescue(async (req, res) => {
+    const { name, ingredients, preparation } = req.body;
+    const { _id } = req.user;
+    const recipe = await Recipes.create({ name, ingredients, preparation, userId: _id });
+    return res.status(StatusCodes.CREATED).json({ recipe });
 }));
 
 module.exports = recipes;
