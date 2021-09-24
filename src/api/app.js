061ac9ae32b2a7e7c775dc335223dd03a9f152ce
tonpasso <wellpasso@gmail.com>
2021-09-24 07:37:03 -0300

    requisito 10

--- src/api/app.js
+++ src/api/app.js
@@ -2,14 +2,18 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+const path = require('path');
 
 const userRouter = require('../controllers/Users');
 const loginRouter = require('../controllers/Login');
 const recipeRouter = require('../controllers/Recipes');
 
+const pathImage = path.resolve(__dirname, '../uploads');
+
 app.use('/users', userRouter);
 app.use('/login', loginRouter);
 app.use('/recipes', recipeRouter);
+app.use('/images', express.static(pathImage));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
