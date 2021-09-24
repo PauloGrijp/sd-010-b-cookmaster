193b0b89abfcb01bde9464737fa80a96ec9bde10
Rafael <rafaeljavaro750@gmail.com>
2021-09-24 14:18:43 -0300

    Cria recipesModel

---
+++ src/api/model/recipesModel.js
@@ -0,0 +1,66 @@
+const { ObjectId } = require('mongodb');
+const connection = require('./connection');
+
+const recipesCollection = 'recipes';
+
+const postNewRecipe = async ({ name, ingredients, preparation }) => {
+  const data = await connection().then((db) =>
+    db.collection(recipesCollection).insertOne({
+      name,
+      ingredients,
+      preparation,
+    }));
+  return data;
+};
+
+const getAllRecipes = async () => {
+  const data = await connection().then((db) => 
+    db.collection(recipesCollection).find({}).toArray());
+  return data;
+};
+
+const getRecipeById = async (id) => {
+  const validId = ObjectId.isValid(id);
+  if (!validId) return;
+  const data = await connection().then((db) =>
+    db.collection(recipesCollection).findOne(ObjectId(id)));
+  return data;
+};
+
+const updateRecepi = async ({ name, ingredients, preparation, id }) => {
+  const validId = ObjectId.isValid(id);
+  if (!validId) return;
+  const data = await connection().then((db) =>
+    db.collection(recipesCollection).updateOne(
+      { _id: ObjectId(id) },
+      { $set: { name, ingredients, preparation } },
+    ));
+  return data;
+};
+
+const deleteRecipeById = async (id) => {
+  const validId = ObjectId.isValid(id);
+  if (!validId) return;
+  const data = await connection().then((db) => 
+    db.collection(recipesCollection).deleteOne({ _id: ObjectId(id) }));
+  return data;
+};
+
+const insertRecipeImage = async (id) => {
+  const data = await connection().then((db) => 
+   db.collection(recipesCollection).updateOne(
+    { _id: ObjectId(id) },
+    { $set: { image: `localhost:3000/src/uploads/${id}.jpeg` } },
+  ));
+  if (!data) return; 
+    const recipeWithImage = await getRecipeById(id);
+    return recipeWithImage;
+};
+
+module.exports = { 
+  postNewRecipe,
+  getAllRecipes,
+  getRecipeById,
+  deleteRecipeById,
+  updateRecepi,
+  insertRecipeImage };
