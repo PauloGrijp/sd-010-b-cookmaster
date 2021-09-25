aa319c24bcb916654194bd72b1185f0a001eaf9f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-25 16:25:00 -0300

    Refactor: Retira os middlewares do arq. app e add nos controllers

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const Login = require('../services/loginServices');
 
 const login = express.Router();
+const validateEmailPassword = require('../middlewares/loginMiddlewares');
 
 const secret = 'minha-senha';
 const jwtConfig = {
@@ -12,16 +13,18 @@ const jwtConfig = {
   algorithm: 'HS256',
 };
 
-login.post('/', rescue(async (req, res) => {
-  const { email: userEmail, password } = req.body;
-  const checkLogin = await Login.findUser(userEmail, password);
-  if (checkLogin.isError) {
-    return res.status(checkLogin.code).json({ message: checkLogin.message });
-  }
-  const { _id, email, role } = checkLogin;
-  const payload = { _id, email, role };
-  const token = jwt.sign(payload, secret, jwtConfig);
-  return res.status(StatusCodes.OK).json({ token });
+login.post('/',
+  validateEmailPassword,
+  rescue(async (req, res) => {
+    const { email: userEmail, password } = req.body;
+    const checkLogin = await Login.findUser(userEmail, password);
+    if (checkLogin.isError) {
+      return res.status(checkLogin.code).json({ message: checkLogin.message });
+    }
+    const { _id, email, role } = checkLogin;
+    const payload = { _id, email, role };
+    const token = jwt.sign(payload, secret, jwtConfig);
+    return res.status(StatusCodes.OK).json({ token });
 })); 
 
 module.exports = login;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,13 +4,16 @@ const rescue = require('express-rescue');
 const userServices = require('../services/userServices');
 
 const usersRouter = express.Router();
+const userValidate = require('../middlewares/userMiddlewares');
 
-usersRouter.post('/', rescue(async (req, res) => {
-  const user = await userServices.createUser(req.body);
-  if (user.isError) {
-    return res.status(user.code).json({ message: user.message });
-  }
-  return res.status(StatusCodes.CREATED).json({ user });
+usersRouter.post('/',
+  userValidate,  
+  rescue(async (req, res) => {
+    const user = await userServices.createUser(req.body);
+    if (user.isError) {
+      return res.status(user.code).json({ message: user.message });
+    }
+    return res.status(StatusCodes.CREATED).json({ user });
 }));
 
 module.exports = usersRouter;
