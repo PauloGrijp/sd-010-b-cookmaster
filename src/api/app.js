7d2ca559f01e769814946af8d4aba950da47fc51
CIGR2021 <appencigr@gmail.com>
2021-09-26 13:31:02 -0300

    alterei o app da pasta api. Colocando o express.json() e as rotas de users

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
 const app = express();
+const users = require('../routes/users');
+
+app.use(express.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/users', users);
 
 module.exports = app;
