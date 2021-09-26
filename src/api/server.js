15f1cadd08c2701570d87a871daf97330f5d8a9b
CIGR2021 <appencigr@gmail.com>
2021-09-26 18:02:52 -0300

    Requisito 3 - Feito

--- src/api/server.js
+++ src/api/server.js
@@ -1,13 +1,15 @@
 const bodyParser = require('body-parser');
 const app = require('./app');
 const { createUser } = require('../controllers/users');
+const { createRecipe } = require('../controllers/recipes');
 const { login } = require('../controllers/login');
+const { validateToken } = require('../middlewares/validateToken');
 
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
-
 app.use(bodyParser.json());
 
 app.post('/users', createUser);
 app.post('/login', login);
+app.post('/recipes', validateToken, createRecipe);
