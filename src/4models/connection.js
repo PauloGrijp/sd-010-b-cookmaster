e079528e90b7308f649e52f69bbb358aefc9fd48
Diegho <dieghonm@gmail.com>
2021-09-25 21:09:25 -0300

    Requisito 9, banco

--- src/4models/connection.js
+++ src/4models/connection.js
@@ -5,12 +5,12 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 // local
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 let DB = null;
 
 // github
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 const connection = () => (DB ? Promise.resolve(DB)
     : MongoClient.connect(MONGO_DB_URL, OPTIONS)
