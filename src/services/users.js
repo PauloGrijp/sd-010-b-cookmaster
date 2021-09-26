faac5dc0e2437aded57431dff4ffaa7fdc714dbf
CIGR2021 <appencigr@gmail.com>
2021-09-26 13:24:36 -0300

    criei o users na pasta services: função createUser.

---
+++ src/services/users.js
@@ -0,0 +1,16 @@
+const validates = require('../middlewares/validateUsers');
+const modelsUsers = require('../models/users');
+
+const createUser = async ( name, email, password ) => {
+  const isValidData = validates.invalidEntries(name, email, password);
+  if(isValidData.message) return isValidData;
+
+  const emailExist = await validates.emailAlreadyRegistered(email);
+  if(emailExist.conflict) return emailExist;
+
+  return modelsUsers.createUser(name, email, password);
+};
+
+module.exports = {
+  createUser,
+};
