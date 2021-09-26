b1132e9c5b954beab1cc83c6c4305234933ffb73
CIGR2021 <appencigr@gmail.com>
2021-09-26 17:55:06 -0300

    Requisito 2 - Feito

--- src/services/users.js
+++ src/services/users.js
@@ -1,4 +1,24 @@
+const jwt = require('jsonwebtoken');
 const { findUserByEmail, create } = require('../models/users');
+
+const SECRET = 'senha1337';
+
+const jwtConfig = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
+
+const findUser = async (email, password) => {
+  const userSearch = await findUserByEmail(email);
+
+  if (!userSearch || userSearch.password !== password) return null;
+
+  const { password: _, ...userWithoutPassword } = userSearch;
+
+  const token = jwt.sign(userWithoutPassword, SECRET, jwtConfig);
+
+  return { token };
+};
 
 const userIsValid = (name, email, password) => {
   if (!name || !email || !password) return false;
@@ -7,14 +27,12 @@ const userIsValid = (name, email, password) => {
 };
 const createUser = async ({ name, email, password }) => {
   const user = await findUserByEmail(email);
-
   if (!userIsValid(name, email, password)) return 'invalid entry';
-
   if (user) return 'user exists';
-
   return create(name, email, password);
 };
 
 module.exports = {
   createUser,
+  findUser,
 };
