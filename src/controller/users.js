7ef913164f0a6a24275f5cab9bf1e7f156ed5b70
CIGR2021 <appencigr@gmail.com>
2021-09-26 13:27:08 -0300

    alterei o users do controller

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,13 +1,17 @@
+const rescue = require('express-rescue');
 const { StatusCodes } = require('http-status-codes');
-
 const servicesUsers = require('../services/users');
 
-const createUser = async (req, res) => {
+const createUser = rescue(async (req, res) => {
   const { name, email, password } = req.body;
+  const newUser = await servicesUsers.createUser({ name, email, password });
 
-  const newUser = await servicesUsers.createUser(name, email, password);
-  res.status(StatusCodes.CREATED).json(newUser);
-};
+  if (newUser.conflict) return res.status(StatusCodes.CONFLICT).json({ message: newUser.message });
+  if (newUser.message) return res.status(StatusCodes.BAD_REQUEST).json(newUser);
+  
+  return res.status(StatusCodes.CREATED).json(newUser);
+
+});
 
 module.exports = {
   createUser,
