70708277c12add3c2d2cd9bcc7e87a6e085c23e0
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-09-27 16:55:30 -0300

    remove uso da biblioteca http-status-codes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,8 @@
-const StatusCodes = require('http-status-codes');
 const userService = require('../services/userService');
+
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const CONFLICT = 409;
 
 // req 1
 const userCreate = async (req, res) => {
@@ -8,14 +11,14 @@ const userCreate = async (req, res) => {
       name, email, password, role });
 
     if (message === 'Invalid entries. Try again.') {
-      return res.status(StatusCodes.BAD_REQUEST).json({ message });
+      return res.status(BAD_REQUEST).json({ message });
     }
 
     if (message === 'Email already registered') {
-      return res.status(StatusCodes.CONFLICT).json({ message });
+      return res.status(CONFLICT).json({ message });
     }
 
-    return res.status(StatusCodes.CREATED)
+    return res.status(CREATED)
       .json({ user: { _id: id, name, email, role: 'user' } });
 };
 
