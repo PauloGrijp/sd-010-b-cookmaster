394a320e2d58a8f135070525deb3f3c9268d2db6
antonioarieiro <sfgfffs@gmail.com>
2021-09-27 10:18:31 -0300

    add router for routes api

--- src/api/app.js
+++ src/api/app.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser').json();
-const Users = require('../controllers/userController');
 
 const app = express();
-
-app.use(bodyParser);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
-
-/* rota para users */
-app.post('/users', Users.createNewUser);
 
 module.exports = app;
--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,8 @@
+const bodyParser = require('body-parser');
 const app = require('./app');
+const usersRouter = require('./Router/usersRouter');
+app.use(bodyParser.json());
 
-const PORT = 3001;
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
