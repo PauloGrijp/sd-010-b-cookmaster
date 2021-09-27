ce532491718ac98346375b5aa092fdd855df428b
antonioarieiro <sfgfffs@gmail.com>
2021-09-27 18:15:45 -0300

    nao entendo esse lint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,9 +11,8 @@ const createUser = async (req, res, _next) => {
   res.status(201).json({ user: { name, email, role, _id } });
 };
 
-const login = async (req, res, next) => {
+const login = async(req, res ,next) => {
   const data = req.body;
-
   if (!data.email || !data.password) {
     return next({
       status: 401,
