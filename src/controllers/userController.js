622525054bef278747bd21a673963042fe5e5530
antonioarieiro <sfgfffs@gmail.com>
2021-09-27 10:58:47 -0300

    controller para criaçao de usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,15 @@
-const rescue = require('express-rescue');
-const User = require('../services/userService');
+const user = require('../models/userModel');
 
-const createNewUser = rescue(async (req, res) => {
-  const { email, name, password } = req.body;
-  // console.log(name, email, password);
-  const user = await User.createUserService(email, name, password);
-  return res.status(201).json(user);
-});
+const createUser = async (req, res, _next) => {
+  const userInfo = req.body;
+  //console.log(data)
+  const result = await user.createNewUser(userInfo);
+  // depois de obeter o resultando da criação na camada model, fiz a desestruturação
+  const { name, email, role, _id } = result;
+  //console.log(name, email, role);
+  res.status(201).json({ user: {name, email, role, _id}});
+};
 
 module.exports = {
-  createNewUser,
-};
+  createUser,
+}
