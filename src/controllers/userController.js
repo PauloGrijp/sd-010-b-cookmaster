42c608aff49b163d7ac0aa5ab060edf5a8776e43
antonioarieiro <sfgfffs@gmail.com>
2021-09-27 11:58:04 -0300

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,7 @@ const createUser = async (req, res, _next) => {
   // depois de obeter o resultando da criação na camada model, fiz a desestruturação
   const { name, email, role, _id } = result;
   // console.log(name, email, role);
-  res.status(201).json({ user: { name, email, role, _id}});
+  res.status(201).json({ user: { name, email, role, _id } });
 };
 
 module.exports = {
