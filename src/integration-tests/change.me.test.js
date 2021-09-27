fbabb4afef7a090e9be693a3ebf0e1bc4ba5ea8d
Diego Serafim <diegoserafim1@gmail.com>
2021-09-26 21:05:31 -0300

    conexao para testes de integração

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -1,2 +1,14 @@
-describe('', () => {
+const chai = require('chai');
+const { expect } = chai;
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+const server = require('../api/app');
+const connectMock = require('./connectMock');
+const usersService = require('../services/usersService');
+
+chai.use(chaiHttp);
+
+describe('POST /users', () => {
+  // ...
 });
