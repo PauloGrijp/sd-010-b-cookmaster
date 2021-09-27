401fbdec4a744e3b0c3e460920b284b9935a4ec6
antonioarieiro <sfgfffs@gmail.com>
2021-09-27 10:57:10 -0300

    rota /users

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const userRoutes = require('../routes/userRoutes');
+
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+// usersRoutes
+app.use('/users', userRoutes);
 
 module.exports = app;
