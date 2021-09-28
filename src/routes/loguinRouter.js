3b6e28b3045568d0f6d11c817c876977963b15cb
antonioarieiro <sfgfffs@gmail.com>
2021-09-28 08:40:10 -0300

    correçoes do lint

--- src/routes/loguinRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const routerLogin = express.Router();
-const { login } = require('../controllers/userController');
-
-routerLogin.post('/', login);  
-
-module.exports = routerLogin;
