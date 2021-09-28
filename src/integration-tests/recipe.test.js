5f563b08f26cee417e158827c4ab0dba1803e93a
tonpasso <wellpasso@gmail.com>
2021-09-28 10:21:40 -0300

    get recipe by id test

--- src/integration-tests/recipe.test.js
+++ src/integration-tests/recipe.test.js
@@ -232,17 +232,108 @@ describe('GET /recipes', () => {
 
 });
 
-describe('GET /recipes/:id', () => {
+describe('GET /recipes/:id', () => {  
+  let connectionMock;
+  let recipeId;
+
+  before(async () => {
+    connectionMock = await mockConnection();
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+
+    await connectionMock
+      .db('Cookmaster')
+      .collection('users')
+      .insertOne({
+        name: "name-fake",
+        email: "email@email.com",
+        password: "12345678",
+        role: "user",
+      });
+
+   const login = await chai
+    .request(app)
+    .post('/login')
+    .send({
+      email: 'email@email.com',
+      password: "12345678",
+    });
+
+   const token = login.body.token;
+
+   const recipe = await chai
+    .request(app)
+    .post('/recipes')
+    .set('authorization', token)
+    .send({
+      name: "fake-name",
+      ingredients: "fake-ing",
+      preparation: "fake-prep"
+    });
+    recipeId = recipe.body.recipe._id;
+
+    console.log(recipeId);
+  });
+
+  after(async () => {
+    MongoClient.connect.restore();
+    await connectionMock
+      .db("Cookmaster")
+      .collection("users")
+      .deleteOne({ email: "email@email.com" });
+    await connectionMock
+      .db("Cookmaster")
+      .collection("recipes")
+      .deleteMany({});
+  });
 
   describe('quando é possível listar uma receita específica', () => {
     let response;
-    let connectionMock;  
 
-    
+    before(async () => {
+      response = await chai.request(app).get(`/recipes/${recipeId}`)      
+    });    
 
+    it('retorna status HTTP 200', () => {
+      expect(response).to.have.status(200);
+    });
+
+    it('retorna um objeto no body', () => {
+      expect(response).to.be.an('object');
+    });
+
+    it('objeto de resposta possui as propriedades "_id" "name", "ingredients", "preparation" e "userId"', () => {
+      expect(response.body).to.have.a.property('_id');
+      expect(response.body).to.have.a.property('name');
+      expect(response.body).to.have.a.property('ingredients');
+      expect(response.body).to.have.a.property('preparation');
+      expect(response.body).to.have.a.property('userId');
+    });    
+  });
+
+  describe('quando não é possível listar uma receita que não existe', () => {
+    let response;
+
+    before(async () => {
+      response = await chai.request(app).get('/recipes/idfake')      
+    });    
+
+    it('retorna status HTTP 404', () => {
+      expect(response).to.have.status(404);
+    });
+
+    it('retorna um objeto no body', () => {
+      expect(response).to.be.an('object');
+    });
+
+     it('objeto de resposta possui uma propriedade chamada "message"', () => {
+      expect(response.body).to.have.a.property('message');
+     });
+
+    it('a propriedade "message" possui uma mensagem de erro adequada', () => {
+      expect(response.body.message).to.be.equal('recipe not found');
+    });
     // it('', () => {});
-
-    // it('', () => {});
+  
     
   })
 })
