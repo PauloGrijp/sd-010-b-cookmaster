c3fc0f4ac78f494345a80ec5bf0933f8e43bb500
andersontrkz <trkz.anderson@gmail.com>
2021-09-28 01:06:10 -0300

    [Altera] Server port

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const PORT = 'mongodb://localhost:27017/Cookmaster';
+const PORT = 'mongodb://mongodb:27017/Cookmaster';
 
 app.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
