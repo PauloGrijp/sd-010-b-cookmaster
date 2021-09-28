dbe4d4cd788125c1f881294efb98c5cf31d286a9
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-27 22:57:39 -0300

    Route to add image on recipe and setting multer

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
 const controllers = require('../controllers');
 const middlewares = require('../middlewares');
+
+const storage = multer.diskStorage({
+  filename: (req, file, callback) => {
+    const fileNameConstruction = `${req.params.id}.jpeg`;
+    callback(null, fileNameConstruction);
+    req.image = `localhost:3000/src/uploads/${fileNameConstruction}`;
+  },
+  destination: (req, file, callback) => {
+    callback(null, path.join(__dirname, '..', 'uploads'));
+  },
+ });
+ const upload = multer({ storage });
 
 const router = express.Router();
 
@@ -17,5 +32,8 @@ router.get('/recipes/:id', controllers.getRecipeByIdController);
 router.put('/recipes/:id', middlewares.validateToken, controllers.putRecipeByIdController);
 
 router.delete('/recipes/:id', middlewares.validateToken, controllers.deleteRecipeByIdController);
+
+router.put('/recipes/:id/image/', 
+middlewares.validateToken, upload.single('image'), controllers.putImageController);
 
 module.exports = router;
