26eecad5046cecb9e5a0a9d3796e4b9a3443201b
aythan azevedo <aythan16@gmail.com>
2021-09-28 19:51:30 -0300

    implements adds

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,6 +2,16 @@ const service = require('../services/users');
 
 async function newUser(req, res) {
   const { name, email, password } = req.body;
+  if (req.route.path === '/users/admin') {
+    const { role } = req.user;
+    if (role !== 'admin') {
+      const error = new Error('Only admins can register new admins');
+      error.code = 403;
+      throw error;
+    }
+    const result = await service.newUser(name, email, password, 'admin');
+    return res.status(201).json({ user: result });
+  }
   const result = await service.newUser(name, email, password, 'user');
   return res.status(201).json({ user: result });
 }
