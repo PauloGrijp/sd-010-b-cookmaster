680a1e76aa3a9363f957984e29dfce983bb28c4a
brunobastosfer <brunobastos@edu.unifor.br>
2021-09-28 23:48:22 -0300

    Recipes Controllers

--- src/api/app.js
+++ src/api/app.js
@@ -5,12 +5,21 @@ const path = require('path');
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (request, file, callback) => callback(null, path
+    .resolve(__dirname, '..', 'uploads')),
+  filename: (request, file, callback) => callback(null, `${request.params.id}.jpeg`),
+});
+
+const upload = multer({ storage });
+
 app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
+app.use('/images', express.static(path.resolve(__dirname, '..', 'uploads')));
 // Não remover esse end-point, ele é necessário para o avaliador
 
 module.exports = app;
--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,1 +1,69 @@
-console.log('olá');
+const { createRecipe,
+  getAll,
+  findById,
+  updateRecipe,
+  deleteRecipe,
+  addImagePath } = require('../models/recipeModel');
+
+  const recipeCreate = async (request, response) => {
+    const { user } = request.user;
+    const { name, ingredients, preparation } = request.body;
+    const { _id: userId } = user;
+
+    const recipe = await createRecipe({ userId, name, ingredients, preparation });
+
+    response.status(201).json({ recipe });
+  };
+
+  const listAllRecipes = async (request, response) => {
+    const { recipes } = await getAll();
+
+    return response.status(200).json(recipes);
+  };
+
+  const findRecipeId = async (request, response) => {
+    const { id } = request.params;
+
+    const { recipe } = await findById({ id });
+
+    return response.status(200).json(recipe);
+  };
+
+  const recipeUpdate = async (request, response) => {
+    const { id } = request.params;
+    const { name, ingredients, preparation } = request.body;
+
+    const { recipe } = await updateRecipe({ id, name, ingredients, preparation });
+
+    return response.status(200).json(recipe);
+  };
+
+  const recipeDelete = async (request, response) => {
+    const { id } = request.params;
+
+    const { recipe } = await deleteRecipe({ id });
+
+    return response.status(204).json(recipe);
+  };
+
+  const addImage = async (req, res) => {
+    const { id } = req.params;
+  
+    const { recipe } = await addImagePath({ id });
+  
+    return res.status(200).json(recipe);
+  };
+
+  const notImg = (error, response, _request, _next) => {
+    response.status(400).send({ error: error.message });
+  };
+
+  module.exports = {
+    recipeCreate,
+    listAllRecipes,
+    findRecipeId,
+    recipeUpdate,
+    recipeDelete,
+    notImg,
+    addImage,
+  };
