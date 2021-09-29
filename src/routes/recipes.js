63dfb4e24deb6e8ab446ea7b7f0084c88f619ad2
Pedro Toselli <phtoselli@gmail.com>
2021-09-29 17:48:48 -0300

    Requisito 9

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-// const upload = require('../middleweres/upload');
+const upload = require('../middleweres/upload');
 const authJWT = require('../middleweres/validateJWT');
-const myMulter = require('../middleweres/upload');
 
 const { create, getAll, getById, remove, update, uploadImg } = require('../controllers/recipes');
 
@@ -18,6 +17,6 @@ router.route('/:id')
   .delete(authJWT, remove);
 
 router.route('/:id/image')
-  .post(authJWT, myMulter.single('image'), uploadImg);
+  .post(authJWT, upload.single('image'), uploadImg);
 
 module.exports = router;
