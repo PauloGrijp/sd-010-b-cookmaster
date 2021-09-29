9d3a39a8181f5a88e4ddf62390c107b3c52a3c8c
Aderson Santos <assaderson@gmail.com>
2021-09-29 11:12:25 -0300

    Requisito 4

--- src/models/recipeModel.js
+++ src/models/recipeModel.js
@@ -2,8 +2,16 @@ const connection = require('./connection');
 
 const create = async (name, ingredients, preparation, user) => {
   const db = await connection();
-  const recipe = await db.collection('recipe').insertOne({ name, ingredients, preparation, user });
+    const recipe = await db.collection('recipes').insertOne({ name, 
+    ingredients,
+preparation,
+userId: user });
   return { recipe: { name, ingredients, preparation, userId: user, _id: recipe.insertedId } };
 };
 
-module.exports = { create };
+const getAll = async () => {
+  const db = await connection();
+  return db.collection('recipes').find({}).toArray();
+};
+
+module.exports = { create, getAll };
