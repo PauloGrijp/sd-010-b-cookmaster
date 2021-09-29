7c7b8d1d8ca622267479e3ee644a30c3862c9b0c
Lucas Ribeiro <lucasrb04@gmail.com>
2021-09-28 22:55:50 -0300

    req 5 corrigindo erro

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -11,7 +11,7 @@ const getRecipeById = async (id) => {
   // Caso nenhum autor seja encontrado, retornamos um objeto de erro.
   if (!recipe) {
     return {
-      number: 405,
+      number: 404,
       error: {
         message: 'recipe not found',
       },
