bd6d172728aaa56e98516e60e5143c0def76b41f
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-30 00:38:01 -0300

    Feat: middleware of uploads image

--- src/middlewares/recipesMiddleware.js
+++ src/middlewares/recipesMiddleware.js
@@ -1,3 +1,5 @@
+const multer = require('multer');
+
 const RecipesSchema = require('../schemas/recipesSchema');
 
 const validateInputs = (req, res, next) => {
@@ -11,6 +13,19 @@ const validateInputs = (req, res, next) => {
   next();
 };
 
+const storage = multer.diskStorage({
+  destination: (_req, _file, callback) => {
+    callback(null, './src/uploads');
+  },
+  filename: (req, _file, callback) => {
+    const { id } = req.params;
+    callback(null, `${id}.jpeg`);
+  },
+});
+
+const uploads = multer({ storage });
+
 module.exports = {
   validateInputs,
+  uploads,
 };
