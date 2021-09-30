324b06f15f5e99a1d21634f603d8c0a7eb0c3609
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-30 00:53:25 -0300

    Refactor: reorder routes by specificity

--- src/router/recipesRoute.js
+++ src/router/recipesRoute.js
@@ -4,11 +4,11 @@ const RecipesController = require('../controllers/recipesController');
 const { tokenValidator } = require('../middlewares/tokenValidator');
 const { validateInputs, uploads } = require('../middlewares/recipesMiddleware');
 
-router.get('/', RecipesController.getAll);
-router.get('/:id', RecipesController.getById);
-router.put('/:id', tokenValidator, RecipesController.update);
 router.put('/:id/image', tokenValidator, uploads.single('image'), RecipesController.uploadPhoto);
-router.post('/', tokenValidator, validateInputs, RecipesController.create);
+router.put('/:id', tokenValidator, RecipesController.update);
+router.get('/:id', RecipesController.getById);
 router.delete('/:id', tokenValidator, RecipesController.deleteRecipe);
+router.post('/', tokenValidator, validateInputs, RecipesController.create);
+router.get('/', RecipesController.getAll);
 
 module.exports = router;
