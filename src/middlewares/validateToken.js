faec1a4719c9c7f328755735d5529e61072c1ea5
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-29 22:07:01 -0300

    testing 9 requirement

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return next({ status: 401, message: 'missing auth token' });
   try {
-    const token = req.headers.authorization;
-    if (!token) return next({ status: 401, message: 'missing auth token' });
 
     const SECRET = 'superSenha';
     
