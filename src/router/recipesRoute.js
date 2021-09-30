7acb18ce9c068288aef531e6bd7e8298ed430cdc
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-30 00:47:54 -0300

    Feat: uploadPhoto route

--- src/router/recipesRoute.js
+++ src/router/recipesRoute.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+
 const RecipesController = require('../controllers/recipesController');
 const { tokenValidator } = require('../middlewares/tokenValidator');
-const { validateInputs } = require('../middlewares/recipesMiddleware');
+const { validateInputs, uploads } = require('../middlewares/recipesMiddleware');
 
 router.get('/', RecipesController.getAll);
 router.get('/:id', RecipesController.getById);
 router.put('/:id', tokenValidator, RecipesController.update);
+router.put('/:id/image', tokenValidator, uploads.single('image'), RecipesController.uploadPhoto);
 router.post('/', tokenValidator, validateInputs, RecipesController.create);
 router.delete('/:id', tokenValidator, RecipesController.deleteRecipe);
 
