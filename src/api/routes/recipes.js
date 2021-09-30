04ddebe24a74174d8b1eca43e62f8d15d367dbf7
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-29 21:44:54 -0300

    Testando o rescue

--- src/api/routes/recipes.js
+++ src/api/routes/recipes.js
@@ -3,9 +3,10 @@ const rescue = require('express-rescue');
 const validationToken = require('../utils/validations/validationsToken');
 const controllers = require('../controllers/controllersRecipes');
 
-route.post('/', rescue(validationToken), rescue(controllers.create));
+route.post('/', rescue(validationToken, controllers.create));
 route.get('/', rescue(controllers.getAll));
 route.get('/:id', rescue(controllers.getById));
-route.put('/:id', rescue(validationToken), rescue(controllers.updateById));
+route.put('/:id', rescue(validationToken, controllers.updateById));
+// route.delete('/:id', rescue())
 
 module.exports = route;
