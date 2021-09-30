44e4490092ac6c2aac540405e5a68201ecb34784
alantanaka <alanctnk@gmail.com>
2021-09-29 21:35:14 -0300

    req 10 test

--- src/api/app.js
+++ src/api/app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use('/users', usersRoute);
 app.use('/login', loginRoute);
 app.use('/recipes', recipeRoute);
+app.use('/images', express.static('src/uploads'));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
