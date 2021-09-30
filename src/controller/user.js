026dada1243e9d9841d041a9704158dae3d2e45d
Paulo Freire <paulofreire.si@outlook.com>
2021-09-30 04:02:22 -0300

    just adding some comments

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,14 +2,17 @@
 const { dbCaller } = require('../models/db');
 
 const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const regexCheck = (email) => regex.test(email);
+const regexCheck = (email) => regex.test(email); // check regex 
 const cogitoErgoSum = (name, email, password) => !name || !password || !email; // check if null
+
+// look for email on db
 const emailCheck = async ({ email }) => {
     const conn = await dbCaller();
     const alreadyExist = await conn.collection('users').findOne({ email });
     return alreadyExist;
 };
 
+// doing all of the user validations
 const userCheck = async ({ name, email, password }) => {
     if (cogitoErgoSum(name, email, password)) {
     return { 
@@ -17,7 +20,7 @@ const userCheck = async ({ name, email, password }) => {
         message: 'Invalid entries. Try again.',
         };
     }
-    if (await emailCheck({ email })) return { code: 409, message: 'Email already registered' };
+    if (await emailCheck({ email })) return { code: 409, message: 'Email already registered' }; // if true return code, else nothing
     if (!regexCheck(email)) return { code: 400, message: 'Invalid entries. Try again.' };
 };
 
