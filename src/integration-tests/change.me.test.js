56d6332131ff26e9c14488de5624f4e9f040f112
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-09-30 21:20:10 -0300

    Checkpoint: finished job day

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -66,6 +66,99 @@ describe('Requisito 11, verifica o endpoint "/users"', () => {
              () => {expect(response.body.user.name).to.be.equal('Janaina')}
        );
    });
+   describe('Quando não são passados os campos "name", "email", "password" no endpoint "/users"', () => {
+        let response = {};
+        const DBServer = new MongoMemoryServer();
 
-   describe('', () => {})
+        before(async () => {
+                const URLMock = await DBServer.getUri();
+                const connectionMock = await MongoClient.connect(URLMock,
+                    { useNewUrlParser: true, useUnifiedTopology: true }
+                );
+
+            sinon.stub(MongoClient, 'connect')
+                .resolves(connectionMock);
+
+        response = await chai.request(server)
+            .post('/users')
+            .send({
+                name: '',
+                email: '',
+                password: ''
+            });
+    });
+
+        after(async () => {
+            MongoClient.connect.restore();
+            await DBServer.stop();
+        });
+
+        it('Retorna o código de status 400', () => {
+            expect(response).to.have.status(400)
+        })
+
+        it('Retorna um objeto', () => {
+            expect(response).to.be.a('object');
+        })
+
+        it('Verifica se não existe a propriedade "message"', () => {
+            expect(response.body).to.have.property('message');
+        })
+
+        it('Verifica se não existe a propriedade "message"', () => {
+            expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+        })
+
+   })
+
+   describe('verifica o endpoint "/login", em caso de falhas', () => {
+        let response = {};
+        const DBServer = new MongoMemoryServer();
+
+        before(async () => {
+                const URLMock = await DBServer.getUri();
+                const connectionMock = await MongoClient.connect(URLMock,
+                    { useNewUrlParser: true, useUnifiedTopology: true }
+                );
+
+                sinon.stub(MongoClient, 'connect')
+                    .resolves(connectionMock);
+
+            response = await chai.request(server)
+                .post('/login')
+                .send({
+                    email: '',
+                    password: ''
+                });
+        });
+
+        after(async () => {
+            MongoClient.connect.restore();
+            await DBServer.stop();
+        });
+
+        it('Retorna o status 401',() => {
+            expect(response).to.have.status(401);
+        })
+
+        it('Retorna um objeto',() => {
+            expect(response).to.be.a('object');
+        })
+
+        it('Verifica se retorna propriedade "message" quando não passado email válido',() => {
+            expect(response.body).to.have.property('message')
+        })
+
+        it('Verifica mensagem de retorno da propriedade "message" quando não passado email válido',() => {
+            expect(response.body.message).to.be.equal('All fields must be filled')
+        })
+        
+        it('Verifica se retorna propriedade "message" quando não passado password válido',() => {
+            expect(response.body).to.have.property('message')
+        })
+
+        it('Verifica mensagem de retorno da propriedade "message" quando não passado password válido',() => {
+            expect(response.body.message).to.be.equal('All fields must be filled')
+        })
+   })
 });
