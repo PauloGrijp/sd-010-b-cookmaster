0caccc23e1800c0ef1173dec2232d703779311b7
Antenor <antenorzapata@gmail.com>
2021-10-06 10:36:22 -0300

    feat: create getAllplants endpoint

--- src/integration-tests/recipes.test.js
+++ src/integration-tests/recipes.test.js
@@ -1,1 +1,14 @@
-const server = require('../api/app');
+// const sinon = require('sinon');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// chai.use(chaiHttp);
+// const { expect } = chai;
+// const { MongoClient } = require('mongodb');
+// const { getConnection } = require('./connectionMock');
+// const server = require('../api/app');
+// const { ObjectId } = require('mongodb');
+// const fs = require('fs');
+
+// describe('test', () => {
+//   expect(1).to.be.equal(2);
+// });
