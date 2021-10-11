dc8b78ce36e7c1850a4155eb5563adc99b31654b
marcos <marcosd3wow@gmail.com>
2021-10-11 10:25:05 -0300

    estruturas de validaçao criadas,iniciando o req 1 e testes locais.

--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -8,13 +8,13 @@ const validate = async (name, email, password) => {
 
   const emailValidation = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 
-  if (emailValidation.test(name) === false) {
+  if (emailValidation.test(email) === false) {
     return {
       err: { status: 400, message: 'Invalid. Try again.' } };
   }
 };
 
-const create = async (name, email, password, role) => {
+const create = async (name, email, password) => {
   const result = await validate(name, email, password);
   if (result) {
     return result;
@@ -26,7 +26,7 @@ const create = async (name, email, password, role) => {
       err: { status: 409, message: 'Email already registered' } };
   }
 
-  const user = await usersModel.create(name, email, password, role);
+  const user = await usersModel.create(name, email, password);
   return { user };
 };
 
