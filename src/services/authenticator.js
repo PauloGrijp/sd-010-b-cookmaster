8cc03dc4dbc6c691039aa672592c3af859e9121d
marcos <marcosd3wow@gmail.com>
2021-10-12 12:54:48 -0300

    tentando resolver o problema do autenticador /2

--- src/services/authenticator.js
+++ src/services/authenticator.js
@@ -11,6 +11,7 @@ const verifyToken = (token) => {
 
 const authenticator = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(authorization);
   if (!authorization) {
   return res.status(401)
   .json({ message: 'missing auth token' }); 
