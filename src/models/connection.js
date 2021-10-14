f93539630f658d6cb54cd72e1336ed6253b04b36
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-14 20:30:44 -0300

    Adiciona conexao ao banco de dados

---
+++ src/models/connection.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = process.env.DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+
+const DB_NAME = process.env.DB_NAME || 'Cookmaster';
+
+const connection = (URL = MONGO_DB_URL) => MongoClient
+    .connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,        
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+    });
+
+module.exports = { connection };
