c65bdf86ce7a098cd47220993188eb0108c9fa44
marcos <marcosd3wow@gmail.com>
2021-10-15 16:43:10 -0300

    estrutura inicial dos testes ok!

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -1,2 +1,13 @@
-describe('', () => {
-});
+// describe('', () => {
+// });
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const { expect } = chai;
+const { MongoClient } = require('mongodb');
+const { getConnection } = require('./connectionMock');
+const server = require('../api/app');
+
+const {user, recipe, anotherRecipe, admin, newAdmin} = require('./mockData')
