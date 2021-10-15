9379cdd116c71b9b0f43c27ac4d4dbe34c19ea57
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-15 00:47:52 -0300

    Tentando mudar a config da connection para parar de bugar o avaliador

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,13 +1,16 @@
 const { MongoClient } = require('mongodb');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
 
-const DB_NAME = process.env.DB_NAME || 'Cookmaster';
+// const DB_NAME = process.env.DB_NAME || 'Cookmaster';
 
-const connection = (URL = MONGO_DB_URL) => MongoClient
-    .connect(URL, {
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+const connection = () => MongoClient
+    .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,        
     })
