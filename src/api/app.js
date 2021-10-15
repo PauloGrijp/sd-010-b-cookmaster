32edf4d90a31c08519d140fccb9a2654491e793d
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-14 21:49:03 -0300

    Adiciona rota login

--- src/api/app.js
+++ src/api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const Login = require('../controllers/login');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
@@ -10,5 +14,7 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.post('/login', Login);
 
 module.exports = app;
