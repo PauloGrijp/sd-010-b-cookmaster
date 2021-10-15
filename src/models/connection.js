73c5412e46a23a36eb54b214346c69cb7e4d2300
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-15 01:19:31 -0300

    Corrige novamente a variavel de ambiente

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
-const MONGO_DB_URL = process.env.DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
