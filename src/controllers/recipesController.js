fd358a4d186a2ec68f7fd8b168d0c0a33e552a32
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-15 00:17:49 -0300

    Cadastra e retorna o recipe

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,7 +1,17 @@
-const createNewRecipe = async (req, res) => {
+const { createNewRecipe } = require('../services/recipeServices');
+
+const newRecipe = async (req, res) => {
+  const { user: { _id }, body: { name, ingredients, preparation } } = req;
+  if (!name || !ingredients || !preparation) {
+    return res
+      .status(400).json({ message: 'Invalid entrie. Try again.' }); 
+  }
   
+  const recipe = await createNewRecipe(_id, name, ingredients, preparation);
+
+  return res.status(201).send(recipe);
 };
 
 module.exports = {
-  createNewRecipe,
+  newRecipe,
 };
