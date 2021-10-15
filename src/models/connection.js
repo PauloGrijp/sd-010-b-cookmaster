dbb19062a61fb6b522a9bc3a8733e35689ff1f92
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-15 01:25:21 -0300

    Done: Readme modified

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require('mongodb');
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'Cookmaster';
 
 const connection = () => MongoClient.connect(MONGO_DB_URL, {
-    // urlNewParser: true,
+    // urlNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then((conn) => conn.db(DB_NAME))
