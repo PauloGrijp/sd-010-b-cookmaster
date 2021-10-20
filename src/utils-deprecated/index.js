a1531d4aa725853f8683440b6ce6849be62ca879
Uibirá <usbarreto@gmail.com>
2021-10-20 19:16:21 -0300

    Retira arquivos obsoletos

--- src/utils-deprecated/index.js
+++
@@ -1,9 +0,0 @@
-// const { validateCredentialsData, validateRecipeData } = require('./validatePayload');
-// const { tokenEncrypt, tokenDecrypt } = require('./validateJWT');
-
-// module.exports = {
-//   validateCredentialsData,
-//   validateRecipeData,
-//   tokenEncrypt,
-//   tokenDecrypt,
-// };
