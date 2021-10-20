a775012c81053baac913a53c20e7cc75ad5719c3
Isabella Paz <bellapaz.iorp@gmail.com>
2021-10-20 13:47:56 -0300

    autorizacao

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,20 +1,24 @@
 const jwt = require('jsonwebtoken');
-const Users = require('../models/usersModels');
+const usersModels = require('../models/usersModels');
 
-const authorization = async (req, res, next) => {
+const secret = 'OMG';
+
+module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
     return res.status(401).json({ message: 'missing auth token' });
   }
   try {
-    const decoded = jwt.verify(token, process.env.SECRET);
-    const user = await Users.findByEmail(decoded.email);
-    delete user.password;
-    req.user = user;
+    const decoded = jwt.verify(token, secret);
+    const user = await usersModels.findByEmail(decoded.email);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: 'Erro ao procurar usuário do token.' });
+    }
+    req.userId = decoded.userId;
     next();
   } catch (err) {
     return res.status(401).json({ message: 'jwt malformed' });
   }
 };
-
-module.exports = authorization;
