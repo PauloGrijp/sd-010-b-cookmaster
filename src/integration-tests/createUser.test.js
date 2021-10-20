2f9e04b2cc5a396be82a44add8a5b577edb68d9f
Lucas Ribeiro <lucasrb04@gmail.com>
2021-10-20 07:44:34 -0300

    req11

--- src/integration-tests/createUser.test.js
+++ src/integration-tests/createUser.test.js
@@ -10,16 +10,15 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('POST /users', () => {
-   describe('quando é criado com sucesso', () => {
+describe('POST /users', function () {
+   describe('quando é criado com sucesso', function () {
        let response = {};
         const DBServer = new MongoMemoryServer();
 
-       before(async () => {
+       before(async function () {
             const URLMock = await DBServer.getUri();
             const connectionMock = await MongoClient.connect(URLMock,
-                { useNewUrlParser: true, useUnifiedTopology: true }
-            );
+                { useNewUrlParser: true, useUnifiedTopology: true });
 
             sinon.stub(MongoClient, 'connect')
                 .resolves(connectionMock);
@@ -29,34 +28,33 @@ describe('POST /users', () => {
                .send({
                    name: 'jane',
                    password: 'senha123',
-                   email: 'oi@oi.com'
+                   email: 'oi@oi.com',
                });
        });
        
-        after(async () => {
+        after(async function () {
             MongoClient.connect.restore();
             await DBServer.stop();
         });
 
-       it('retorna o código de status 201', () => {
+       it('retorna o código de status 201', function () {
            expect(response).to.have.status(201);
        });
 
-       it('retorna um objeto', () => {
+       it('retorna um objeto', function () {
            expect(response.body).to.be.a('object');
        });
 
-       it('o objeto possui a propriedade "user"', () => {
+       it('o objeto possui a propriedade "user"', function () {
            expect(response.body).to.have.property('user');
        });
        
-      it('o objeto "user" possui a propriedade "name"', () => {
+      it('o objeto "user" possui a propriedade "name"', function () {
           expect(response.body.user).to.have.property('name');
       });
 
-      it('o objeto "user" possui a propriedade "email"', () => {
+      it('o objeto "user" possui a propriedade "email"', function () {
           expect(response.body.user).to.have.property('email');
       });
-
    });
 });
