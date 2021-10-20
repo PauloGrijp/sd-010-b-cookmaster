ab8276c3e78b1a2c086a8d88930064a04eeb0435
Isabella Paz <bellapaz.iorp@gmail.com>
2021-10-20 13:30:47 -0300

    erro no login

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -18,7 +18,7 @@ module.exports = rescue(async (req, res, next) => {
   const { error } = loginSchema.validate(req.body);
   if (error) return next(error);
   const { email, password } = req.body;
-  const user = await loginServices.login({ email, password });
+  const user = await loginServices({ email, password });
   if (user.error) return next(user.error);
   const token = jwt.sign(user, process.env.SECRET);
   res.status(200).json({ token });
