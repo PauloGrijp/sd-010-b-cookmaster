847b964aef37ad12c4162d0526e19472b21d4e9f
Ricardo <ricardo.rals.dev@gmail.com>
2021-10-21 07:58:21 -0300

    Agora foi

--- src/integration-tests/connectionMock.js
+++ src/integration-tests/connectionMock.js
@@ -1,16 +1,10 @@
 const { MongoClient } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
-const OPTIONS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+const DBServer = new MongoMemoryServer();
+const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 
-const getConnection = async () => {
-  const DBServer = await MongoMemoryServer.create();
-  const URLMock = DBServer.getUri();
-
-  return MongoClient.connect(URLMock, OPTIONS);
-};
+const getConnection = () => DBServer.getUri()
+  .then((URLMock) => MongoClient.connect(URLMock, OPTIONS));
 
 module.exports = getConnection;
