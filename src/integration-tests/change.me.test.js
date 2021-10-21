b68481479e3d42902a19539bc1d0030996920b58
Ricardo <ricardo.rals.dev@gmail.com>
2021-10-20 22:25:16 -0300

    testando 60%

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -27,9 +27,9 @@ describe('Testando a rota /users', () => {
       response = await chai.request(server)
         .post('/users')
         .send({
-            name: 'Ricardo Augusto',
-            email: 'trybe@betrybe.com',
-            password: 'senha123'
+          name: 'Ricardo Augusto',
+          email: 'trybe@betrybe.com',
+          password: 'senha123'
         });
     });
 
@@ -43,28 +43,28 @@ describe('Testando a rota /users', () => {
     });
 
     it('Retorna um objeto', () => {
-        expect(response).to.be.an('object');
+      expect(response).to.be.an('object');
     });
 
     it('O Objeto contém chave "user"', () => {
-        expect(response.body).to.have.property('user');
+      expect(response.body).to.have.property('user');
     });
 
     it('O Objeto contem as keys "name", "email", "role"', () => {
-        expect(response.body.user).to.have.all.keys('name', 'email', 'role');
+      expect(response.body.user).to.have.all.keys('name', 'email', 'role');
     });
-    
+
     it('O objeto não está vazio', () => {
-        expect(response.body).to.be.not.empty;
+      expect(response.body).to.be.not.empty;
     })
 
-    
+
   });
 });
 
 describe('Testando a rota /login', () => {
-    describe('Usuário logado com sucesso', () => {
-        let response = {};
+  describe('Usuário logado com sucesso', () => {
+    let response = {};
     const DBServer = new MongoMemoryServer();
 
     before(async () => {
@@ -80,9 +80,9 @@ describe('Testando a rota /login', () => {
       response = await chai.request(server)
         .post('/users')
         .send({
-            name: 'Ricardo Augusto',
-            email: 'trybe@betrybe.com',
-            password: 'senha123'
+          name: 'Ricardo Augusto',
+          email: 'trybe@betrybe.com',
+          password: 'senha123'
         });
     });
 
@@ -92,50 +92,167 @@ describe('Testando a rota /login', () => {
     });
 
     it('Permite efetuar login com o usuário criado anteriormente', async () => {
-        response = await chai.request(server)
+      response = await chai.request(server)
         .post('/login')
         .send({
-            email: 'trybe@betrybe.com',
-            password: 'senha123'
+          email: 'trybe@betrybe.com',
+          password: 'senha123'
         });
-  
-        expect(response).to.have.status(200);
-        expect(response.body).to.be.a('object');
-        expect(response.body).to.have.property('token');
-      });
-  
-      it('Não permite realizar o login com a senha errada', async () => {
-        response = await chai.request(server)
+
+      expect(response).to.have.status(200);
+      expect(response.body).to.be.a('object');
+      expect(response.body).to.have.property('token');
+    });
+
+    it('Não permite realizar o login com a senha errada', async () => {
+      response = await chai.request(server)
         .post('/login')
         .send({
-            email: 'trybe@betrybe.com',
-            password: 'senha123'
+          email: 'trybe@betrybe.com',
+          password: 'senha123'
         });
-  
-        expect(response).to.have.status(401);
-        expect(response.body).to.be.a('object');
-        expect(response.body).to.have.property('message');
-      });
-  
-      it('"message" possui o valor "Incorrect username or password"', () => {
-          expect(response.body.message).to.be.equal('Incorrect username or password');
-      });
-  
-      it('Não permite realizar o login com o email errado', async () => {
-        response = await chai.request(server)
+
+      expect(response).to.have.status(401);
+      expect(response.body).to.be.a('object');
+      expect(response.body).to.have.property('message');
+    });
+
+    it('"message" possui o valor "Incorrect username or password"', () => {
+      expect(response.body.message).to.be.equal('Incorrect username or password');
+    });
+
+    it('Não permite realizar o login com o email errado', async () => {
+      response = await chai.request(server)
         .post('/login')
         .send({
-            email: 'whind@tests.com',
-            password: 'senha123'
+          email: 'whind@tests.com',
+          password: 'senha123'
         });
-  
-        expect(response).to.have.status(401);
-        expect(response.body).to.be.a('object');
-        expect(response.body).to.have.property('message');
-      });
-  
-      it('"message" possui o valor "Incorrect username or password"', () => {
-          expect(response.body.message).to.be.equal('Incorrect username or password');
-      });
-    })
+
+      expect(response).to.have.status(401);
+      expect(response.body).to.be.a('object');
+      expect(response.body).to.have.property('message');
+    });
+
+    it('"message" possui o valor "Incorrect username or password"', () => {
+      expect(response.body.message).to.be.equal('Incorrect username or password');
+    });
+  })
 })
+
+
+
+describe('Testando a rota /recipes', () => {
+  describe('Usuário logado com sucesso', () => {
+    let response = {};
+    const DBServer = new MongoMemoryServer();
+
+    before(async () => {
+      const URLMock = await DBServer.getUri();
+      const OPTIONS = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      };
+      const connectionMock = await MongoClient.connect(URLMock, OPTIONS);
+
+      sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+
+      response = await chai.request(server)
+        .post('/users')
+        .send({
+          name: 'Ricardo Augusto',
+          email: 'trybe@betrybe.com',
+          password: 'senha123'
+        });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+
+    it('Permite criar uma receita se estiver logado', async () => {
+      response = await chai.request(server)
+        .post('/login')
+        .send({
+          email: 'whind@tests.com.br',
+          password: 'senhaSuperForte'
+        });
+
+      response = await chai.request(server)
+        .post('/recipes')
+        .set('authorization', response.body.token)
+        .send({
+          name: 'Burguer',
+          ingredients: 'Blend bovino 120g, cheddar, bacon',
+          preparation: 'Frie everything'
+        });
+
+      expect(response).to.have.status(201);
+      expect(response.body).to.be.a('object');
+      expect(response.body).to.have.property('recipe');
+
+    });
+
+    it('Permite buscar todas as receitas sem estar logado', async () => {
+      response = await chai.request(server)
+        .get('/recipes')
+
+      expect(response).to.have.status(200);
+      expect(response.body).to.be.be.an('array');
+
+    });
+
+    it('Permite buscar uma receita por id sem estar logado', async () => {
+      response = await chai.request(server)
+        .post('/login')
+        .send({
+          email: 'whind@tests.com.br',
+          password: 'senhaSuperForte'
+        });
+
+      response = await chai.request(server)
+        .post('/recipes')
+        .set('authorization', response.body.token)
+        .send({
+          name: 'Burguer',
+          ingredients: 'Blend bovino 120g, cheddar, bacon',
+          preparation: 'Frie everything'
+        });
+
+      response = await chai.request(server)
+        .get(`/recipes/${response.body.recipe._id}`)
+
+      expect(response).to.have.status(200);
+      expect(response.body).to.be.a('object');
+      expect(response.body).to.have.property('ingredients');
+
+    });
+
+    it('Permite deletar uma receita se estiver logado', async () => {
+      response = await chai.request(server)
+        .post('/login')
+        .send({
+          email: 'whind@tests.com.br',
+          password: 'senhaSuperForte'
+        });
+
+      const token = response.body.token;
+
+      response = await chai.request(server)
+        .post('/recipes')
+        .set('authorization', token)
+        .send({
+          name: 'Burguer',
+          ingredients: 'Blend bovino 120g, cheddar, bacon',
+          preparation: 'Frie everything'
+        });
+
+      response = await chai.request(server)
+        .delete(`/recipes/${response.body.recipe._id}`)
+        .set('authorization', token)
+
+      expect(response).to.have.status(204);
+    });
+  });
+});
