cabe3e58110d9f951f83a568684eb99d65ee07e2
Lucas Ribeiro <lucasrb04@gmail.com>
2021-10-25 14:11:17 -0300

    Revert "req 10 acessando imagem"
    This reverts commit de5b38a26667c053224e181b08fa3e2e9ee8ff7f.

--- src/models/connection.js
+++ src/models/connection.js
@@ -6,8 +6,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; // for local
-const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // for github 
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; // for local
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // for github 
 
 const DB_NAME = 'Cookmaster';
 
